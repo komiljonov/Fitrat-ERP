091e14f3ed79fcb044cb436afdb8303ec4b7abc1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-01-27 17:03:56 +0500

    student group app, schedules

--- config/data/account/views.py
+++ config/data/account/views.py
@@ -78,7 +78,7 @@ class CustomAuthToken(TokenObtainPairView):
             'user_id': user.pk,
             'phone': user.phone,
             'role': user.role,
-            'filial': user.filial,
+            'filial': user.filial.name,
         }, status=status.HTTP_200_OK)
 
 
