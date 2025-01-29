1853d546451def21c914a36954400a3727c321e7
shokh_smee <shohjahonobruyev3@gmail.com>
2025-01-29 20:20:03 +0500

    results

--- config/data/account/views.py
+++ config/data/account/views.py
@@ -78,7 +78,7 @@ class CustomAuthToken(TokenObtainPairView):
             'user_id': user.pk,
             'phone': user.phone,
             'role': user.role,
-            'filial': user.filial.id,
+            # 'filial': user.filial.id,
         }, status=status.HTTP_200_OK)
 
 
