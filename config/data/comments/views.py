d4602d9c69b474f1804f165dfe8f0067c2993bf0
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-14 19:12:42 +0500

    parents app

--- config/data/comments/views.py
+++ config/data/comments/views.py
@@ -16,6 +16,7 @@ class CommentListCreateAPIView(ListCreateAPIView):
     serializer_class = CommentSerializer
     # permission_classes = (IsAuthenticated,)
 
+
 class CommentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -35,3 +36,17 @@ class CommentLidRetrieveListAPIView(ListAPIView):
             return Comment.objects.filter((Q(lid__id=id) | Q(student__id=id)), creator__id=creator)
         return Comment.objects.none()
 
+class CommentAppRead(ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+    def get_queryset(self):
+        id = self.kwargs.get('pk')
+        creator = self.request.query_params.get('creator')
+        if id:
+            return Comment.objects.filter(Q(lid__id=id) | Q(student__id=id)).order_by('-created_at')
+
+        if creator:
+            return Comment.objects.filter((Q(lid__id=id) | Q(student__id=id)), creator__id=creator).order_by('-created_at')
+        return Comment.objects.none()
