830dd9c344dead5da10206d51d96fc63b5801a08
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-14 19:12:42 +0500

    parents app

--- config/data/comments/urls.py
+++ config/data/comments/urls.py
@@ -2,12 +2,12 @@
 from django.urls import path
 from .views import (CommentListCreateAPIView,
                     CommentRetrieveUpdateDestroyAPIView,
-                    CommentLidRetrieveListAPIView)
+                    CommentLidRetrieveListAPIView, CommentAppRead)
 
 urlpatterns = [
     path('', CommentListCreateAPIView.as_view(), name='comment-list-create'),
     path("<uuid:pk>/", CommentRetrieveUpdateDestroyAPIView.as_view(), name='comment-retrieve-update'),
 
     path('all/<uuid:pk>/', CommentLidRetrieveListAPIView.as_view(), name='comment-list-create'),
-    # path('student/<uuid:pk>/', CommentStudentRetrieveListAPIView.as_view(), name='comment-list-create'),
+    path('app/<uuid:pk>/', CommentAppRead.as_view(), name='comment-list-create'),
 ]
