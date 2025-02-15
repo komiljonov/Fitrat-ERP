97b34526885be564c86d381a6007946574d31a48
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-15 11:34:11 +0500

    notifications commented

--- config/data/comments/urls.py
+++ config/data/comments/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import (CommentListCreateAPIView,
                     CommentRetrieveUpdateDestroyAPIView,
-                    CommentLidRetrieveListAPIView, CommentAppRead)
+                    CommentLidRetrieveListAPIView, CommentAppRead, CommentStuff, CommentLid)
 
 urlpatterns = [
     path('', CommentListCreateAPIView.as_view(), name='comment-list-create'),
@@ -10,4 +10,7 @@ urlpatterns = [
 
     path('all/<uuid:pk>/', CommentLidRetrieveListAPIView.as_view(), name='comment-list-create'),
     path('app/<uuid:pk>/', CommentAppRead.as_view(), name='comment-list-create'),
+
+    path('stuff/', CommentStuff.as_view(), name='comment-list-create'),
+    path('stuff/<uuid:pk>/', CommentLid.as_view(), name='comment-list-create'),
 ]
--- config/data/comments/views.py
+++ config/data/comments/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 
-from .models import Comment
-from .serializers import CommentSerializer
+from .models import Comment, StuffComments
+from .serializers import CommentSerializer, CommentStuffSerializer
 from ..lid.new_lid.models import Lid
 from ..student.student.models import Student
 
@@ -50,3 +50,24 @@ class CommentAppRead(ListAPIView):
         if creator:
             return Comment.objects.filter((Q(lid__id=id) | Q(student__id=id)), creator__id=creator).order_by('-created_at')
         return Comment.objects.none()
+
+
+
+class CommentStuff(ListCreateAPIView):
+    serializer_class = CommentStuffSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = StuffComments.objects.all()
+
+    def get_queryset(self):
+        id = self.kwargs.get('pk')
+        creator = self.request.query_params.get('creator')
+        if id:
+            return StuffComments.objects.filter(stuff__id=id)
+        if creator:
+            return StuffComments.objects.filter(stuff__id=id,creator__id=creator)
+        return StuffComments.objects.none()
+
+class CommentLid(RetrieveUpdateDestroyAPIView):
+    queryset = CommentStuffSerializer.objects.all()
+    serializer_class = CommentStuffSerializer
+    permission_classes = (IsAuthenticated,)
