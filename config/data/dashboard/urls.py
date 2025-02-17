2c795e1f37cc637fa4ae8e5c0de3996b536b5e18
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-17 16:50:22 +0500

    notifications commented

--- config/data/dashboard/urls.py
+++ config/data/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-from data.dashboard.views import DashboardView, MarketingChannels
+from data.dashboard.views import DashboardView, MarketingChannels,Room_place
 
 urlpatterns = [
     path('admin',DashboardView.as_view(), name='dashboard'),
     path("channels",MarketingChannels.as_view(), name='marketing-channels'),
+    path("rooms",Room_place.as_view(), name='rooms'),
 ]
