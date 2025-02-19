3905d3abf2194b563b86ebe07eb994c4916b425b
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-19 18:07:40 +0500

    lid task corrected

--- config/data/notifications/send_notifications.py
+++ config/data/notifications/send_notifications.py
@@ -1,23 +1,30 @@
 import time
 from pydoc_data.topics import topics
+from uuid import UUID
 
 import firebase_admin
 import icecream
 from firebase_admin import credentials, messaging, exceptions
+from icecream import ic
 
 cred = credentials.Certificate("data/notifications/send_notif.json")
 firebase_admin.initialize_app(cred)
 
 
-def send_push(title, msg,topics : str,dataObj=None):
-    icecream.ic(topics)
+def send_push(title, msg, topics, dataObj=None):
+    ic(topics)
+
+    # Ensure topics is a list of strings
+    if isinstance(topics, (str, UUID)):
+        topics = [str(topics)]  # Convert UUID to string and wrap it in a list
+
     message = messaging.MulticastMessage(
         notification=messaging.Notification(
             title=title,
             body=msg,
         ),
         data=dataObj,
-        tokens=list(topics),
+        tokens=topics,  # Now topics is guaranteed to be a list of strings
     )
 
     response = messaging.send_multicast(message)
