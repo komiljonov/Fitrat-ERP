4e2915f5948c8ceb0ceea6f82f00c5d511df7ad9
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-19 17:47:06 +0500

    lid task corrected

--- config/data/notifications/signals.py
+++ config/data/notifications/signals.py
@@ -11,13 +11,13 @@ def on_attendance_create(sender, instance: Notification, created, **kwargs):
         instance.delete()  # Directly delete the instance
         return
 
-# @receiver(post_save, sender=Notification)
-# def on_attendance_update(sender, instance: Notification, created, **kwargs):
-#     """Send push notification when a new notification is created."""
-#     if created:
-#         fcm.send_push(
-#             title="Notification !",
-#             msg=instance.comment,
-#             topics=f"user_{instance.user.pk}",  # Ensure the topic is passed as a string
-#         )
-#         print(f"Notification sent to user_{instance.user.pk}")
+@receiver(post_save, sender=Notification)
+def on_attendance_update(sender, instance: Notification, created, **kwargs):
+    """Send push notification when a new notification is created."""
+    if created:
+        fcm.send_push(
+            title="Notification !",
+            msg=instance.comment,
+            topics=f"user_{instance.user.pk}",  # Ensure the topic is passed as a string
+        )
+        print(f"Notification sent to user_{instance.user.pk}")
