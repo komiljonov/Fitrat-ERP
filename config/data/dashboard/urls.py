669dc795b7def1205e4c9c47dc5977ba8942b226
shokh_smee <shohjahonobruyev3@gmail.com>
2025-02-26 17:38:43 +0500

    changes for web schedule

--- config/data/dashboard/urls.py
+++ config/data/dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from data.dashboard.views import DashboardView, MarketingChannels, Room_place, DashboardLineGraphAPIView, \
-    MonitoringView, DashboardWeeklyFinanceAPIView
+    MonitoringView, DashboardWeeklyFinanceAPIView, ArchivedView
 
 urlpatterns = [
     path('admin/',DashboardView.as_view(), name='dashboard'),
@@ -10,6 +10,8 @@ urlpatterns = [
     path("finance/",DashboardLineGraphAPIView.as_view(), name='dashboard-line-graph'),
 
     path('monitoring/',MonitoringView.as_view(), name='monitoring'),
+
+    path("archive/",ArchivedView.as_view(), name='archived'),
 
     path('paychart/',DashboardWeeklyFinanceAPIView.as_view(), name='paychart'),
 
