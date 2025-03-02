dacce8026401479e5e69aed6e51e1c476e431628
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-02 12:07:19 +0500

    changes for web schedule

--- config/data/student/studentgroup/views.py
+++ config/data/student/studentgroup/views.py
@@ -70,15 +70,8 @@ class GroupStudentList(ListAPIView):
         """
         Fetch the students related to a specific group from the URL path parameter.
         """
-        status = self.request.query_params.get('status')
         group_id = self.kwargs.get('pk')
         queryset = StudentGroup.objects.filter(group__id=group_id)
-
-        if status:
-            queryset = queryset.filter(student__isnull=False,
-                                       student__balance_status=status)
-        elif status == "":
-            queryset = queryset.filter(student__isnull=False)
 
         return queryset
 
