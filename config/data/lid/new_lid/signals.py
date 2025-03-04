3a615dd4d3b333e9afc74408cc67f6efb9927ea7
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-04 16:33:17 +0500

    changes for web schedule

--- config/data/lid/new_lid/signals.py
+++ config/data/lid/new_lid/signals.py
@@ -1,10 +1,38 @@
+from django.db import transaction
 from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from ..new_lid.models import Lid
+from ...account.models import CustomUser
 from ...parents.models import Relatives
 from ...student.attendance.models import Attendance
 from ...student.student.models import Student
+
+
+@receiver(pre_save, sender=Lid)
+def on_pre_save(sender, instance, **kwargs):
+    """
+    Before saving a Lid instance, check if:
+    - The lid_stage_type is changing from 'NEW_LID' to 'ORDERED_LID'
+    - The call_operator field is None, then assign the first available call operator.
+    """
+    if not instance._state.adding:  # Ensure it's an update, not a new instance
+        # Fetch previous value of `lid_stage_type` before update
+        previous_instance = sender.objects.get(pk=instance.pk)
+
+        if (
+                previous_instance.lid_stage_type == "NEW_LID"
+                and instance.lid_stage_type == "ORDERED_LID"
+                and instance.call_operator is None
+        ):
+            call_operator = CustomUser.objects.filter(role="CALL_OPERATOR").first()
+            if call_operator:
+                instance.call_operator = call_operator  # Corrected assignment
+
+                # Ensure atomic transaction to avoid partially updated data
+                with transaction.atomic():
+                    instance.save(update_fields=["call_operator"])
 
 
 @receiver(post_save, sender=Lid)
@@ -22,14 +50,13 @@ def on_details_create(sender, instance: Lid, created, **kwargs):
             import hashlib
             password_hash = hashlib.sha256("1".encode()).hexdigest()
 
-
             student, student_created = Student.objects.get_or_create(
                 phone=instance.phone_number,
                 defaults={
                     "first_name": instance.first_name,
                     "last_name": instance.last_name,
                     "photo": instance.photo,
-                    "password" : password_hash,
+                    "password": password_hash,
                     "middle_name": instance.middle_name,
                     "date_of_birth": instance.date_of_birth,
                     "education_lang": instance.education_lang,
@@ -66,7 +93,7 @@ def on_details_create(sender, instance: Lid, created, **kwargs):
                 student.save()
 
             # Update attendance records
-            Attendance.objects.filter(lid=instance).update(student=student,lid=None)
+            Attendance.objects.filter(lid=instance).update(student=student, lid=None)
 
             Relatives.objects.filter(lid=instance).update(student=student)
             # Archive the Lid
