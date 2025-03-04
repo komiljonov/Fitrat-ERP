9f6a99a1337d81a3af3b5536a5a248cce62c2bdf
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-04 21:07:23 +0500

    changes for web schedule

--- config/data/lid/new_lid/models.py
+++ config/data/lid/new_lid/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.utils import timezone
 
@@ -9,107 +8,105 @@ from ...department.marketing_channel.models import MarketingChannel
 from ...stages.models import NewLidStages, NewOredersStages
 from ...upload.models import File
 
+
 class Lid(TimeStampModel):
+    sender_id = models.CharField(max_length=120, null=True, blank=True)
+    message_text = models.CharField(max_length=120, null=True, blank=True)
 
-    sender_id = models.CharField(max_length=120,null=True,blank=True)
-    message_text = models.CharField(max_length=120,null=True,blank=True)
-
-    photo : "File" = models.ForeignKey('upload.File',
-                                       on_delete=models.SET_NULL,
-                                       null=True,blank=True,
-                                       related_name='lids_photo')
+    photo: "File" = models.ForeignKey('upload.File',
+                                      on_delete=models.SET_NULL,
+                                      null=True, blank=True,
+                                      related_name='lids_photo')
 
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100,null=True,blank=True)
-    middle_name = models.CharField(max_length=100,null=True,blank=True)
-    phone_number = models.CharField(max_length=100,unique=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, unique=True)
     date_of_birth = models.DateField(default=timezone.now())
 
-    language_choise = (("ENG","ENG"),
-                       ("RU","RU"),
-                       ("UZB","UZB"))
+    language_choise = (("ENG", "ENG"),
+                       ("RU", "RU"),
+                       ("UZB", "UZB"))
 
-    education_lang = models.CharField(choices=language_choise,default="UZB",max_length=100)
+    education_lang = models.CharField(choices=language_choise, default="UZB", max_length=100)
     student_type = models.CharField(max_length=100, default="student")
     edu_class = models.CharField(choices=[
-        ("SCHOOL","School"),
-        ("UNIVERSITY","University"),
-        ("NONE","None"),
-    ],default="NONE",
+        ("SCHOOL", "School"),
+        ("UNIVERSITY", "University"),
+        ("NONE", "None"),
+    ], default="NONE",
         max_length=100,
         help_text="Education level at school if student studies at school")
-    edu_level = models.CharField(null=True,blank=True,max_length=100)
-    subject = models.ForeignKey("subject.Subject",on_delete=models.SET_NULL,null=True,blank=True,
+    edu_level = models.CharField(null=True, blank=True, max_length=100)
+    subject = models.ForeignKey("subject.Subject", on_delete=models.SET_NULL, null=True, blank=True,
                                 help_text="Subject that student won at competition")
     ball = models.IntegerField(default=0, null=True, blank=True, help_text="Earned ball at competition")
 
-    filial : Filial = models.ForeignKey(Filial, on_delete=models.SET_NULL,
-                                        null=True, blank=True,
-                                        help_text="Filial for this student")
+    filial: Filial = models.ForeignKey(Filial, on_delete=models.SET_NULL,
+                                       null=True, blank=True,
+                                       help_text="Filial for this student")
 
-    marketing_channel : MarketingChannel = models.ForeignKey(MarketingChannel,
-                                         on_delete=models.SET_NULL,
-                                         null=True, blank=True,
-                                         help_text="Marketing channel for this student")
+    marketing_channel: MarketingChannel = models.ForeignKey(MarketingChannel,
+                                                            on_delete=models.SET_NULL,
+                                                            null=True, blank=True,
+                                                            help_text="Marketing channel for this student")
 
     lid_stage_type = models.CharField(
         choices=(
-        ("NEW_LID","NEW_LID"),
-        ("ORDERED_LID","ORDERED_LID"),
+            ("NEW_LID", "NEW_LID"),
+            ("ORDERED_LID", "ORDERED_LID"),
         ),
-        max_length=100,default="NEW_LID"
+        max_length=100, default="NEW_LID"
     )
     lid_stages = models.CharField(choices=[
-        ("YANGI_LEAD","YANGI_LEAD"),
-        ("KUTULMOQDA","KUTULMOQDA"),
+        ("YANGI_LEAD", "YANGI_LEAD"),
+        ("KUTULMOQDA", "KUTULMOQDA"),
     ],
         max_length=100,
         help_text="LID's YANGI_LEAD stage type",
-        null=True,blank=True
+        null=True, blank=True
     )
     is_expired = models.BooleanField(default=False)
     ordered_stages = models.CharField(
         choices=[
-            ("KUTULMOQDA","KUTULMOQDA"),
-            ("BIRINCHI_DARSGA_KELMAGAN","BIRINCHI_DARSGA_KELMAGAN"),
-            ("BIRINCHI_DARS_BELGILANGAN","BIRINCHI_DARS_BELGILANGAN"),
-            ("YANGI_BUYURTMA","YANGI_BUYURTMA"),
+            ("KUTULMOQDA", "KUTULMOQDA"),
+            ("BIRINCHI_DARSGA_KELMAGAN", "BIRINCHI_DARSGA_KELMAGAN"),
+            ("BIRINCHI_DARS_BELGILANGAN", "BIRINCHI_DARS_BELGILANGAN"),
+            ("YANGI_BUYURTMA", "YANGI_BUYURTMA"),
         ],
         max_length=100,
         help_text="LID's YANGI_LEAD stage type",
-        null=True,blank=True
+        null=True, blank=True
     )
-    is_archived = models.BooleanField(default=False,help_text="Is this student archived or not")
+    is_archived = models.BooleanField(default=False, help_text="Is this student archived or not")
 
-    is_dubl = models.BooleanField(default=False,help_text="Is this student duble or not")
+    is_dubl = models.BooleanField(default=False, help_text="Is this student duble or not")
 
-    is_frozen = models.BooleanField(default=False,help_text="Is this student frozen or not")
+    is_frozen = models.BooleanField(default=False, help_text="Is this student frozen or not")
 
-    call_operator : "CustomUser" = models.ForeignKey("account.CustomUser",
-                                                     on_delete=models.SET_NULL,
-                                                     null=True, blank=True,
-                                                     help_text="CallOperator for this lid",
-                                                     related_name="call_operator")
+    call_operator: "CustomUser" = models.ForeignKey("account.CustomUser",
+                                                    on_delete=models.SET_NULL,
+                                                    null=True, blank=True,
+                                                    help_text="CallOperator for this lid",
+                                                    related_name="call_operator", limit_choices_to={
+            "is_call_operator": True,
+        })
 
-    is_student = models.BooleanField(default=False,help_text="Is this student or not")
+    is_student = models.BooleanField(default=False, help_text="Is this student or not")
 
-    service_manager : "CustomUser" = models.ForeignKey("account.CustomUser",
-                                                 on_delete=models.SET_NULL,
-                                                 null=True, blank=True,
-                                                 related_name='service_manager')
+    service_manager: "CustomUser" = models.ForeignKey("account.CustomUser",
+                                                      on_delete=models.SET_NULL,
+                                                      null=True, blank=True,
+                                                      related_name='service_manager')
 
-    lid_stages : models.QuerySet['NewLidStages']
-    ordered_stages : models.QuerySet['NewOredersStages']
+    lid_stages: models.QuerySet['NewLidStages']
+    ordered_stages: models.QuerySet['NewOredersStages']
 
     sales_manager: "CustomUser" = models.ForeignKey('account.CustomUser',
                                                     on_delete=models.CASCADE, null=True, related_name="sales_manager")
 
-    file : "File" = models.ManyToManyField("upload.File",
-                                           related_name="lid_file",null=True,blank=True)
-
+    file: "File" = models.ManyToManyField("upload.File",
+                                          related_name="lid_file", null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.subject} {self.ball} in {self.lid_stages} stage"
-
-
-
