72d8074331a1378798658cb955ed5a2449ad1de7
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-05 23:00:00 +0500

    changes for web schedule

--- config/data/dashboard/views.py
+++ config/data/dashboard/views.py
@@ -365,7 +365,7 @@ class MonitoringView(APIView):
         if full_name:
             teachers = teachers.filter(name__icontains=full_name)
         if filial:
-            teachers = teachers.filter(filial_id=filial)
+            teachers = teachers.filter(filial__id=filial)
         if start_date and end_date:
             teachers = teachers.filter(created_at__date__gte=start_date, created_at__lte=end_date)
         elif start_date:
