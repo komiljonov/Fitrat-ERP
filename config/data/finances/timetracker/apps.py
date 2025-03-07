7c2adc16cf621647667a30e84dbb5a61677f8a6b
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-07 20:52:22 +0500

    dfdsf

--- config/data/finances/timetracker/apps.py
+++ config/data/finances/timetracker/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TimetrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timetracker'
+    name = 'data.finances.timetracker'
