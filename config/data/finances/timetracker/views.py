1788f4003c5ac4f87e5b39568d668e0a7feb1a03
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-07 21:00:57 +0500

    dfdsf

--- config/data/finances/timetracker/views.py
+++ config/data/finances/timetracker/views.py
@@ -15,7 +15,18 @@ class AttendanceList(ListCreateAPIView):
     def get_queryset(self):
         filial = self.request.query_params.get('filial')
         id = self.request.query_params.get('id')
+        action = self.request.query_params.get('action')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
         queryset = Employee_attendance.objects.all()
+
+        if start_date:
+            queryset = queryset.filter(created_at__gte=start_date)
+        if end_date:
+            queryset = queryset.filter(created_at__lte=end_date)
+
+        if action:
+            queryset = queryset.filter(action=action)
         if filial:
             queryset = queryset.filter(filial__id=filial)
         if id:
