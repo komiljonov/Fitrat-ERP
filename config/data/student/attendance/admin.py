4378a72546b6d7328b598df3a127d1528992dcb9
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-07 20:19:57 +0500

    dfdsf

--- config/data/student/attendance/admin.py
+++ config/data/student/attendance/admin.py
@@ -6,6 +6,6 @@ from .models import Attendance
 # Register your models here.
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ( 'lid','student','reason')
+    list_display = ( 'lid','student',"group",'reason')
     list_filter = ('reason',)
     search_fields = ('reason',)
