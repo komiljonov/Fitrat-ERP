2e346a56e50fe4e79aced3dac4209389bbbd655a
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-07 20:43:39 +0500

    dfdsf

--- config/data/finances/timetracker/admin.py
+++ config/data/finances/timetracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Employee_attendance
+
+
 # Register your models here.
+
+
+@admin.register(Employee_attendance)
+class FinansAdmin(admin.ModelAdmin):
+    list_display = ('user__full_name', 'action','is_there',"is_gone")
+    search_fields = ('user__full_name', 'action')
+    list_filter = ('user__full_name', 'action','is_there',"is_gone")
+
