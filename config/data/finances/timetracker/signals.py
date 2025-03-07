d4d5d6417f663eff338c1a29df4f0e5a8559200a
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-07 21:33:17 +0500

    dfdsf

---
+++ config/data/finances/timetracker/signals.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Employee_attendance
+from ...account.models import CustomUser
+
+
+@receiver(post_save, sender=Employee_attendance)
+def on_create(sender, instance: Employee_attendance, created, **kwargs):
+    if created :
+
+        if instance.user and instance.action == "In_office":
+            user = CustomUser.objects.get(id=instance.user.id)
+            if instance.time > user.enter:
+                instance.type = "Late"
+                instance.save()
+            if instance.time < user.enter:
+                instance.type = "On_time"
+                instance.save()
+
+
+
+
+
+
+
+
