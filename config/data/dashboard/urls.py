b9c5e30b293bf39d959aee73b610758de2fcfdd1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-14 14:52:31 +0500

    dfdsf

--- config/data/dashboard/urls.py
+++ config/data/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 from data.dashboard.views import DashboardView, MarketingChannels, CheckRoomFillingView, DashboardLineGraphAPIView, \
     MonitoringView, DashboardWeeklyFinanceAPIView, ArchivedView, MonitoringExcelDownloadView, SalesApiView, \
-    FinanceStatisticsApiView, StudentLanguage, ExportDashboardToExcelAPIView
+    FinanceStatisticsApiView, StudentLanguage, ExportDashboardToExcelAPIView, AdminLineGraph
 
 urlpatterns = [
     path('admin/',DashboardView.as_view(), name='dashboard'),
@@ -25,4 +25,6 @@ urlpatterns = [
     path("student-lang/",StudentLanguage.as_view(), name='student-lang'),
 
     path("finance-excel/",ExportDashboardToExcelAPIView.as_view(), name='finance-excel'),
+
+    path("admin-linegraph/",AdminLineGraph.as_view(), name='admin-linegraph'),
 ]
