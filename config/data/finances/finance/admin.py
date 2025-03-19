59f4210c714edc25ed1ea9126950199ed392c473
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-19 22:43:49 +0500

    kpi

--- config/data/finances/finance/admin.py
+++ config/data/finances/finance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Finance
+from .models import Finance, SaleStudent
 
 
 # Register your models here.
@@ -10,3 +10,10 @@ class FinansAdmin(admin.ModelAdmin):
     list_display = ("casher__name", "casher__role","action",'amount','kind')
     search_fields = ("action",'kind',)
     list_filter = ("action",'kind',)
+
+
+@admin.register(SaleStudent)
+class SaleStudentAdmin(admin.ModelAdmin):
+    list_display = ("creator__full_name","sale__name","sale__amount","expired_date", "comment", "created_at")
+    search_fields = ("sale__name","sale__amount")
+    list_filter = ("sale__amount",)
