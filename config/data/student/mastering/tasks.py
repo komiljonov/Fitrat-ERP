b289579c3c191901f9b86a4352a6fbfd2cf1170c
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-23 20:48:49 +0500

    kpi for accountant task

--- config/data/student/mastering/tasks.py
+++ config/data/student/mastering/tasks.py
@@ -2,9 +2,13 @@ import logging
 from datetime import datetime, timedelta
 from celery import shared_task
 from django.utils.timezone import now
+from icecream import ic
+
 from ..lesson.models import ExtraLesson
 from ..student.models import Student
 from ..studentgroup.models import StudentGroup
+from ...account.models import CustomUser
+from ...finances.compensation.models import Bonus, Compensation
 from ...finances.finance.models import KpiFinance
 from ...teachers import teacher
 
@@ -14,7 +18,7 @@ logging.basicConfig(level=logging.INFO)
 def check_monthly_extra_lessons():
     today = now().date()
 
-    start_of_month = today.replace(day=1)  # First day of the month
+    start_of_month = today.replace(day=1)
 
     next_month = (today.replace(day=28) + timedelta(days=4)).replace(day=1)
     end_of_month = next_month - timedelta(days=1)  # Last day of the current month
@@ -45,21 +49,135 @@ def check_monthly_extra_lessons():
     return list(teachers)
 
 
-
-#  4. Moliya menejeri
-# Har bir qarzdor bo’lmagan va active o’quvchi uchun bonus #
-# rejani (jami active va jami yangi o’quvchilar ro’yxati hisobga olinadi)   93 - 94.9, 95%- 97.9% , 98% - 99.9%, 100%  har bitasi uchun alohida summa belgilanadi. Agar 95 da kam bosa bonus berilmiydi
-# foizlar activedagi 50% dan kop to’lagan o’quvchilardan kelib chiqan holda hisoblanadi va yangi o’quvchilar esa qarzor bo’masligi kere
-#
-# Jarima; qarzdorlar soni 85% - 80.1% ,  80%-70.1% gacha 70< bosa jarima belgilanadi
-
 @shared_task
 def check_accountant_kpi():
     today = now().date()
 
     start_of_month = today.replace(day=1)  # First day of the month
-
     next_month = (today.replace(day=28) + timedelta(days=4)).replace(day=1)
     end_of_month = next_month - timedelta(days=1)  # Last day of the current month
-    active_student = Student.objects.filter(balance_status="ACTIVE",
-                                            student_stage_type="ACTIVE_STUDENT").count()
+
+    # Get accountants for each filial
+    accountants = CustomUser.objects.filter(role="ACCOUNTING")
+
+    for accountant in accountants:
+        # Get active students with no debt
+        active_students = Student.objects.filter(balance_status="ACTIVE", student_stage_type="ACTIVE_STUDENT")
+
+        if active_students.count() > 0:
+            bonuses = Bonus.objects.filter(user=accountant, name="Har bir qarzdor bo’lmagan va Aktiv o'quvchi uchun bonus")
+
+            for bonus in bonuses:
+                total_kpi = active_students.count() * bonus.amount
+                for student in active_students:
+                    KpiFinance.objects.create(
+                        user=accountant,
+                        student=student,
+                        type=f"{start_of_month.month} oyi uchun {active_students.count()} ta active"
+                             f" va qarzdor bulmagan o'quvchi uchun {total_kpi} sum bonus!",
+                        amount=total_kpi
+                    )
+
+                logging.info(f"📊 Monthly Task: {total_kpi} for {active_students.count()} no-debt active students for {accountant}")
+
+
+        students = Student.objects.filter(is_archived=False,)
+
+        if students.count() > 0:
+            active_no_debt_students = students.filter(balance__gte=300000)
+            new_no_debt_students = students.filter(balance_status="ACTIVE", balance__gte=0)
+
+            active_percentage = ((active_no_debt_students.count() + new_no_debt_students.count()) / students.count()) * 100
+
+            if active_percentage <95:
+                bonus = Bonus.objects.filter(user=accountant,name="Jami yangi va aktiv o'quvchi o'quvchilarning "
+                                                                  "93% dan 94.9% gacha bo'lgan qismi uchun bonus").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami yangi va aktiv o'quvchi o'quvchilarning "
+                                                                  "93% dan 94.9% gacha bo'lgan qismi uchun bonus",
+                    amount=bonus.amount,
+                    type="INCOME",
+                )
+
+            if active_percentage == 100:
+                bonus = Bonus.objects.filter(user=accountant, name="Jami yangi va aktiv o'quvchi o'quvchilarning "
+                                                                   "100% gacha bo'lgan qismi uchun bonus").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami yangi va aktiv o'quvchi o'quvchilarning "
+                                                                   "100% gacha bo'lgan qismi uchun bonus",
+                    amount=bonus.amount,
+                    type="INCOME",
+                )
+
+            if active_percentage >= 98 and active_percentage <= 99.9:
+                bonus = Bonus.objects.filter(user=accountant, name="Jami yangi va aktiv o'quvchi o'quvchilarning"
+                                                                   " 98% dan 99.9% gacha bo'lgan qismi uchun bonus").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami yangi va aktiv o'quvchi o'quvchilarning 98% dan 99.9% gacha bo'lgan qismi uchun bonus",
+                    amount=bonus.amount,
+                    type="INCOME",
+                )
+
+            if active_percentage >=95 and active_percentage <=97.9:
+                bonus = Bonus.objects.filter(user=accountant, name="Jami yangi va aktiv o'quvchi o'quvchilarning "
+                                                                   "95% dan 97.9% gacha bo'lgan qismi uchun bonus").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami yangi va aktiv o'quvchi o'quvchilarning 95% dan 97.9% gacha bo'lgan qismi uchun bonus",
+                    amount=bonus.amount,
+                    type="INCOME",
+                )
+
+        debt = Student.objects.filter(balance_status="INACTIVE",balance__lte=0, is_archived=False)
+        if debt.count() > 0:
+            debt_percentage = (debt.count() / students.count()) * 100
+
+            if debt_percentage > 0 and debt_percentage <= 70:
+                comp = Compensation.objects.filter(user=accountant, name="Jami qarzdor o'quvchilar sonining "
+                                                                         "70% dan kichik bo'lgan qismi (Jarima)").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami qarzdor o'quvchilar sonining 70% dan kichik bo'lgan qismi (Jarima)",
+                    amount=comp.amount,
+                    type="EXPENSE",
+                )
+
+            if debt_percentage >=70.1 and debt_percentage <=80:
+                comp = Compensation.objects.filter(user=accountant, name="Jami qarzdor o'quvchilar sonining 70% "
+                                                                         "dan 80.1% gacha bo'lgan qismi (Jarima)").first()
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami qarzdor o'quvchilar sonining 70% dan 80.1% gacha bo'lgan qismi (Jarima)",
+                    amount=comp.amount,
+                    type="EXPENSE",
+                )
+
+            if debt_percentage >=80.1 and debt_percentage <=85:
+                comp = Compensation.objects.filter(user=accountant,name="Jami qarzdor o'quvchilar sonining"
+                                                                        " 80.1% dan 85% gacha bo'lgan qismi (Jarima)")
+                KpiFinance.objects.create(
+                    user=accountant,
+                    lid=None,
+                    student=None,
+                    reason="Jami qarzdor o'quvchilar sonining 80.1% dan 85% gacha bo'lgan qismi (Jarima)",
+                    amount=comp.amount,
+                    type="EXPENSE",
+                )
+
+        logging.info(f"for {accountant.full_name} months kpi calculation ended ... ")
+    logging.info(f"months kpi calculation ended ... ")
