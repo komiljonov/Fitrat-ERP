349ab5149f22ec3fd8b62210808940c4aa5a9ee3
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-27 12:00:28 +0500

    student error

--- config/data/finances/compensation/models.py
+++ config/data/finances/compensation/models.py
@@ -44,7 +44,7 @@ class Asos(BaseModel):
     name = models.CharField(max_length=256)
 
     class Meta:
-        ordering = None
+        ordering = "created_at"
 
     def __str__(self):
         return f"{self.name} "
