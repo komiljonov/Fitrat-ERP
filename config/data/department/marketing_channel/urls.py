acd61a248565b666cfad7bbabe33b3b22a9af9d3
shokh_smee <shohjahonobruyev3@gmail.com>
2025-03-29 17:33:29 +0500

    schedule finish date if conditions

--- config/data/department/marketing_channel/urls.py
+++ config/data/department/marketing_channel/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import MarketingChannelList,MarketingChannelNOPG,MarketingChannelDetail
+from .views import MarketingChannelList, MarketingChannelNOPG, MarketingChannelDetail, GroupTypeList, GroupTypeDetail
 
 urlpatterns = [
     path("", MarketingChannelList.as_view(), name="marketing_channel_list"),
     path("<uuid:pk>/", MarketingChannelDetail.as_view(), name="marketing_channel_detail"),
     path("no-pg/", MarketingChannelNOPG.as_view(), name="marketing_channelNOPG"),
+
+
+
+    path("group/",GroupTypeList.as_view(), name="group_type_list"),
+    path("group/<uuid:pk>/",GroupTypeDetail.as_view(), name="group_type_detail"),
 ]
