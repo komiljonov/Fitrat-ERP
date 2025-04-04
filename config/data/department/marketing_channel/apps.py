7e8a8e49c1fcc8a777f82f88a6111f64436216b2
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-04 20:54:30 +0500

    settings

--- config/data/department/marketing_channel/apps.py
+++ config/data/department/marketing_channel/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MarketingChannelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.department.marketing_channel'
+
+    def ready(self):
+        import data.department.marketing_channel.signals
