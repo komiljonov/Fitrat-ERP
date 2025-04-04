abb64535f3bf91a59d3bb18305ac0c354a39a876
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-04 18:28:50 +0500

    settings

--- config/root/celery.py
+++ config/root/celery.py
@@ -10,38 +10,38 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    # 'send-daily-messages': {
-    #     'task': 'data.tasks.tasks.check_daily_tasks',
-    #     'schedule': crontab(minute='*/1'),
-    # },
-    # 'check-daily-leads': {
-    #     'task': 'data.lid.new_lid.tasks.check_daily_leads',
-    #     'schedule': crontab(minute='*/1'),
-    # },
-    # "check_attendance_daily": {
-    #     "task": "data.finances.timetracker.tasks.check_daily_user_attendance",
-    #     "schedule": crontab(hour=0, minute=0),
-    # },
-    # 'check-monthly-extra-lessons': {
-    #     'task': 'data.student.mastering.tasks.check_monthly_extra_lessons',
-    #     'schedule': crontab(day_of_month=1, hour=0, minute=0),
-    # },
-    # 'send_daily_excel_report': {
-    #     'task': "data.dashboard.tasks.send_daily_excel_report",
-    #     'schedule': crontab(hour=9, minute=0),
-    # },
-    # "check_accountant_kpi": {
-    #     "task": "data.student.mastering.tasks.check_accountant_kpi",
-    #     "schedule": crontab(day_of_month=28, hour=0, minute=0),
-    # },
-    # "check_monthly_asos5": {
-    #     "task": "data.finance.compensation.tasks.check_monthly_student_catching_monitoring",
-    #     "schedule": crontab(day_of_month=28, hour=0, minute=0),
-    # },
-    # "activate_group" : {
-    #     "task": "data.student.groups.tasks.activate_group",
-    #     "schedule": crontab(hour=0, minute=0),
-    # },
+    'send-daily-messages': {
+        'task': 'data.tasks.tasks.check_daily_tasks',
+        'schedule': crontab(minute='*/1'),
+    },
+    'check-daily-leads': {
+        'task': 'data.lid.new_lid.tasks.check_daily_leads',
+        'schedule': crontab(minute='*/1'),
+    },
+    "check_attendance_daily": {
+        "task": "data.finances.timetracker.tasks.check_daily_user_attendance",
+        "schedule": crontab(hour=0, minute=0),
+    },
+    'check-monthly-extra-lessons': {
+        'task': 'data.student.mastering.tasks.check_monthly_extra_lessons',
+        'schedule': crontab(day_of_month=1, hour=0, minute=0),
+    },
+    'send_daily_excel_report': {
+        'task': "data.dashboard.tasks.send_daily_excel_report",
+        'schedule': crontab(hour=9, minute=0),
+    },
+    "check_accountant_kpi": {
+        "task": "data.student.mastering.tasks.check_accountant_kpi",
+        "schedule": crontab(day_of_month=28, hour=0, minute=0),
+    },
+    "check_monthly_asos5": {
+        "task": "data.finance.compensation.tasks.check_monthly_student_catching_monitoring",
+        "schedule": crontab(day_of_month=28, hour=0, minute=0),
+    },
+    "activate_group" : {
+        "task": "data.student.groups.tasks.activate_group",
+        "schedule": crontab(hour=0, minute=0),
+    },
     "check_attendance_manager_kpi" : {
         "task": "data.student.mastering.tasks.check_attendance_manager_kpi",
         "schedule": crontab(minute="*/1"),
