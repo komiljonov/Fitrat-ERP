59804932af0da6ece345d58bc1d6a5a765d60a97
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-04 20:23:02 +0500

    settings

--- config/data/finances/finance/signals.py
+++ config/data/finances/finance/signals.py
@@ -85,14 +85,17 @@ def on_create(sender, instance: VoucherStudent, created, **kwargs):
 @receiver(post_save, sender=KpiFinance)
 def on_create(sender, instance: KpiFinance, created, **kwargs):
     if created:
-        Finance.objects.create(
-            casher = Casher.objects.filter(filial__in=instance.filial.first(),
-                                           role__in=["ADMINISTRATOR", "ACCOUNTANT"]).first(),
-            action = "EXPENSE",
-            amount = instance.amount,
-            kind = Kind.objects.filter(name="Bonus").first(),
-            payment_method="card",
-            stuff = instance.user,
-            comment = "Xodim uchun kpi sifatida qo'shildi!"
-        )
+        for filial in instance.filial.all():
+            Finance.objects.create(
+                casher=Casher.objects.filter(
+                    filial=filial,
+                    role__in=["ADMINISTRATOR", "ACCOUNTANT"]
+                ).first(),
+                action="EXPENSE",
+                amount=instance.amount,
+                kind=Kind.objects.filter(name="Bonus").first(),
+                payment_method="card",
+                stuff=instance.user,
+                comment="Xodim uchun KPI sifatida qo'shildi!"
+            )
 
