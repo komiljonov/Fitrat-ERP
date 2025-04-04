ca3952bfec30371cc83f6c6316192192ab32d7ba
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-04 18:15:53 +0500

    settings

--- config/root/celery.py
+++ config/root/celery.py
@@ -44,19 +44,19 @@ app.conf.beat_schedule = {
     },
     "check_attendance_manager_kpi" : {
         "task": "data.student.mastering.tasks.check_attendance_manager_kpi",
-        "schedule": crontab(day_of_month=1,hour=0, minute=0),
+        "schedule": crontab(day_of_month=4,hour=18, minute=18),
     },
     "check_filial_manager_kpi": {
         "task": "data.student.mastering.tasks.check_filial_manager_kpi",
-        "schedule": crontab(day_of_month=1, hour=0, minute=0),
+        "schedule": crontab(day_of_month=4, hour=18, minute=18),
     },
     "check_filial_director_kpi": {
         "task": "data.student.mastering.tasks.check_filial_director_kpi",
-        "schedule": crontab(day_of_month=1, hour=0, minute=0),
+        "schedule": crontab(day_of_month=4, hour=18, minute=18),
     },
     "check_monitoring_manager_kpi": {
         "task": "data.student.mastering.tasks.check_monitoring_manager_kpi",
-        "schedule": crontab(day_of_month=1, hour=0, minute=0),
+        "schedule": crontab(day_of_month=4, hour=18, minute=18),
     },
 }
 
