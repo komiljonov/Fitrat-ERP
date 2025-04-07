ed7045b151455826d5f4751e195225fb5ab24519
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-07 19:23:55 +0500

    results change

--- config/data/student/attendance/signals.py
+++ config/data/student/attendance/signals.py
@@ -110,7 +110,10 @@ def on_attendance_money_back(sender, instance: Attendance, created, **kwargs):
 
         # DAILY PAYMENT TYPE
         if sale:
+            ic(sale.amount)
+            ic(price * (sale.amount / 100))
             price = price - price * (sale.amount / 100)
+            ic(price)
         bonus_amount = price * bonus_percent / Decimal("100")
         income_amount = price - bonus_amount
 
@@ -190,7 +193,10 @@ def on_attendance_money_back(sender, instance: Attendance, created, **kwargs):
                 # DAILY PAYMENT TYPE
             per_lesson_price = price / lesson_count
             if sale:
+                ic(sale.amount)
+                ic(per_lesson_price * (sale.amount / 100))
                 per_lesson_price = per_lesson_price - per_lesson_price * (sale.amount / 100)
+                ic(per_lesson_price)
 
             instance.amount = per_lesson_price
             instance.save()
