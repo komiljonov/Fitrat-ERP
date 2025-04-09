556a5cac7f66bcb3fd5a77ca85619eec3c8dfb44
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-09 16:33:20 +0500

    results change

--- config/data/results/apps.py
+++ config/data/results/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ResultsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.results'
+
+    def ready(self):
+        import data.results.signals
