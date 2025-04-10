d3102c6299478e0fa742bd205a74b0794bbfb464
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-10 17:12:57 +0500

    commit

--- config/data/student/appsettings/urls.py
+++ config/data/student/appsettings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import StoresListView,StoreDetailView
+from .views import StoresListView,StoreDetailView, StudentHomeView
 
 urlpatterns = [
     path('store/',StoresListView.as_view()),
     path('store/<uuid:pk>',StoreDetailView.as_view()),
+
+    path('home/',StudentHomeView.as_view())
 ]
--- config/data/student/appsettings/views.py
+++ config/data/student/appsettings/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from ...student.course.models import Course
+from ...student.student.models import Student
+from ...student.studentgroup.models import StudentGroup
+
+from rest_framework.response import Response
 
 from .serializers import StoresSerializer
 from .models import Store
@@ -30,3 +37,28 @@ class StoreDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = StoresSerializer
     permission_classes = [IsAuthenticated]
 
+class StudentHomeView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        
+        user = self.request.user
+
+        avg_progress = Student.objects.filter(
+            user = user
+        )
+
+        in_progress_courses = Course.objects.filter(
+            group__in=StudentGroup.objects.filter(
+                group__status='ACTIVE', student__user=user
+            ).values_list('group', flat=True)
+        ).distinct()
+        
+        in_progress_courses_counts = in_progress_courses.count()
+
+
+        return Response( {
+            "avg_progress" : avg_progress,
+            "in_progress_courses" : in_progress_courses,
+            "in_progress_courses_counts" : in_progress_courses_counts
+        }
+        ) 
