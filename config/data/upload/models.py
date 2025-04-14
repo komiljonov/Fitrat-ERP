fe736d383023d18a7e23ac44469ef2bb70411b53
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-14 15:00:00 +0500

    results change

--- config/data/upload/models.py
+++ config/data/upload/models.py
@@ -5,6 +5,11 @@ from ..command.models import BaseModel
 
 
 class File(BaseModel):
+    choice = models.CharField(choices=[
+        ("file", "File"),
+        ("link", "Link"),
+    ],default="file",max_length=120, null=True, blank=True)
+    url = models.URLField(max_length=255, null=True, blank=True)
     file = models.FileField(upload_to="files/", null=True, blank=True)
 
 
--- config/data/upload/serializers.py
+++ config/data/upload/serializers.py
@@ -10,6 +10,8 @@ class FileUploadSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'file',
+            "choice",
+            "url"
         ]
 
     def to_representation(self, instance):
