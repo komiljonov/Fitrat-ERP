1445daa954d7cbb574c8068d9b070ca388f244ff
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-15 18:03:48 +0500

    results change

--- config/data/student/appsettings/urls.py
+++ config/data/student/appsettings/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import StoresListView,StoreDetailView, StudentHomeView
+from .views import StoresListView, StoreDetailView, StudentHomeView, StudentDetailView, FinanceListView
 
 urlpatterns = [
     path('store/',StoresListView.as_view()),
     path('store/<uuid:pk>',StoreDetailView.as_view()),
+    path('home/',StudentHomeView.as_view()),
 
-    path('home/',StudentHomeView.as_view())
+    path("student/<uuid:id>/",StudentDetailView.as_view()),
+    path("finance/<uuid:id>/",FinanceListView.as_view()),
 ]
