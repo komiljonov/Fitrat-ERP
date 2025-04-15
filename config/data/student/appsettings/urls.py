e26f4d4e41f632f01e70b6cfe07217b2da0042bc
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-15 21:06:25 +0500

    results change

--- config/data/student/appsettings/urls.py
+++ config/data/student/appsettings/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('store/<uuid:pk>',StoreDetailView.as_view()),
     path('home/',StudentHomeView.as_view()),
 
-    path("student/<uuid:id>/",StudentDetailView.as_view()),
-    path("finance/<uuid:id>/",FinanceListView.as_view()),
+    path("student/<uuid:pk>",StudentDetailView.as_view()),
+    path("finance/<uuid:pk>",FinanceListView.as_view()),
 ]
