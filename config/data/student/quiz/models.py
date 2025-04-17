f6068bcbbb539c56c105c3c8f656229ebaff6b01
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-17 19:31:28 +0500

    import api

--- config/data/student/quiz/models.py
+++ config/data/student/quiz/models.py
@@ -42,3 +42,44 @@ class Question(BaseModel):
             for i in range(4): # Add dummy answers if no answers exist
                 Answer.objects.create(text=f"Answer {i+1}", is_correct=False)
 
+
+
+class Vocabulary(BaseModel):
+    quiz = models.ForeignKey("quiz.Quiz", on_delete=models.SET_NULL, null=True,blank=True,related_name='vocabularies_quiz')
+    photo = models.ForeignKey("upload.File", on_delete=models.SET_NULL, null=True, blank=True, related_name='vocabulary_photo')
+    voice = models.ForeignKey("upload.File", on_delete=models.SET_NULL, null=True, blank=True, related_name='vocabulary_voice')
+    in_english = models.CharField(max_length=255, null=True,blank=True)
+    in_uzbek = models.CharField(max_length=255, null=True,blank=True)
+    def __str__(self):
+        return f"{self.quiz.name}    {self.in_english}    {self.in_uzbek}"
+
+
+class Fill_gaps(BaseModel):
+    quiz : "Quiz" = models.ForeignKey("quiz.Quiz", on_delete=models.SET_NULL, null=True,blank=True,related_name='fill_gaps_quiz')
+    question : "Question" = models.ForeignKey("quiz.Question", on_delete=models.SET_NULL, null=True, blank=True,related_name='fill_gaps_question')
+
+    def __str__(self):
+        return f"{self.quiz.title}    {self.question.question_text}"
+
+
+class Listening(BaseModel):
+    quiz : "Quiz" = models.ForeignKey("quiz.Quiz", on_delete=models.SET_NULL, null=True,blank=True,related_name='listening_quiz')
+    question : "Question" = models.ForeignKey("quiz.Question", on_delete=models.SET_NULL, null=True, blank=True,related_name='listening_question')
+
+    def __str__(self):
+        return f"{self.quiz.title}    {self.question.question_text}"
+
+
+class Pairs(BaseModel):
+    first_pair = models.CharField(max_length=255)
+    second_pair = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_pair}    {self.second_pair}"
+
+
+class MatchPairs(BaseModel):
+    quiz : "Quiz" = models.ForeignKey("quiz.Quiz", on_delete=models.SET_NULL, null=True,blank=True,related_name='match_pairs_quiz')
+    pairs : "Pairs" = models.ManyToManyField(Pairs)
+    def __str__(self):
+        return f"{self.quiz.title} "
--- config/data/student/quiz/urls.py
+++ config/data/student/quiz/urls.py
@@ -5,7 +5,8 @@ from .views import (
     QuestionListCreateView,
     QuestionRetrieveUpdateDestroyView,
     QuestionCheckAnswerView,
-    QuizBulkCheckView, QuizImportView
+    QuizBulkCheckView, QuizImportView, FillGapsView, FillGapsDetailsView, VocabularyListView, VocabularyDetailsView,
+    PairsListView, PairsDetailsView, MatchPairsListView, MatchPairsDetailsView
 )
 
 urlpatterns = [
@@ -17,4 +18,17 @@ urlpatterns = [
     path('<uuid:quiz_pk>/check-bulk/', QuizBulkCheckView.as_view(), name='quiz-check-bulk'),
 
     path('import-quiz/', QuizImportView.as_view(), name='import-quiz'),
+
+    path("fill_gaps/",FillGapsView.as_view(), name='fill-gaps' ),
+    path("fill_gaps/<uuid:pk>", FillGapsDetailsView.as_view(), name='fill-gaps'),
+
+    path("vocabularies/", VocabularyListView.as_view(), name='vocabulary-list'),
+    path("vocabularies/<uuid:pk>", VocabularyDetailsView.as_view(), name='vocabulary-detail'),
+
+    path("pairs/",PairsListView.as_view(), name='pairs'),
+    path("pairs/<uuid:pk>", PairsDetailsView.as_view(), name='pairs-uuid'),
+
+    path("match-pairs/", MatchPairsListView.as_view(), name='match-pairs'),
+    path("match-pairs/<uuid:pk>", MatchPairsDetailsView.as_view(), name='match-pairs'),
+
 ]
