996287840ec317324428bec52e2d4207b0205d05
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-19 19:23:33 +0500

    docker compose changes

--- config/data/student/quiz/urls.py
+++ config/data/student/quiz/urls.py
@@ -2,22 +2,24 @@ from django.urls import path
 from .views import (
     QuizListCreateView,
     QuizRetrieveUpdateDestroyView,
-    QuestionListCreateView,
-    QuestionRetrieveUpdateDestroyView,
-    QuestionCheckAnswerView,
-    QuizBulkCheckView, QuizImportView, FillGapsView, FillGapsDetailsView, VocabularyListView, VocabularyDetailsView,
-    PairsListView, PairsDetailsView, MatchPairsListView, MatchPairsDetailsView, ExamListView, ExamDetailsView
+    QuestionRetrieveUpdateDestroyView, FillGapsView, FillGapsDetailsView, VocabularyListView, VocabularyDetailsView,
+    LeftPairsListView, LeftPairsDetailsView, MatchPairsListView, MatchPairsDetailsView, ExamListView, ExamDetailsView,
+    QuizGapsListView, QuizGapsDetailsView, QuestionsListCreateView, AnswerListCreateView,
+    AnswerRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
     path('', QuizListCreateView.as_view(), name='quiz-list'),
     path('<uuid:pk>/', QuizRetrieveUpdateDestroyView.as_view(), name='quiz-detail'),
-    path('<uuid:quiz_pk>/questions/', QuestionListCreateView.as_view(), name='question-list'),
-    path('<uuid:quiz_pk>/questions/<uuid:pk>/', QuestionRetrieveUpdateDestroyView.as_view(), name='question-detail'),
-    path('<uuid:quiz_pk>/questions/<uuid:pk>/check/', QuestionCheckAnswerView.as_view(), name='question-check'),
-    path('<uuid:quiz_pk>/check-bulk/', QuizBulkCheckView.as_view(), name='quiz-check-bulk'),
 
-    path('import-quiz/', QuizImportView.as_view(), name='import-quiz'),
+    path("questions/", QuestionsListCreateView.as_view(), name="question-list"),
+    path("questions/<uuid:pk>/", QuestionRetrieveUpdateDestroyView.as_view(), name="question-detail"),
+
+    path("answers/", AnswerListCreateView.as_view(), name="exam-list"),
+    path("answers/<uuid:pk>/", AnswerRetrieveUpdateDestroyView.as_view(), name="exam-detail"),
+
+    path("quiz-questions/", QuizGapsListView.as_view(), name="quiz-question-list"),
+    path("quiz-questions/<uuid:pk>/", QuizGapsDetailsView.as_view(), name="quiz-question-detail"),
 
     path("fill_gaps/",FillGapsView.as_view(), name='fill-gaps' ),
     path("fill_gaps/<uuid:pk>", FillGapsDetailsView.as_view(), name='fill-gaps'),
@@ -25,8 +27,8 @@ urlpatterns = [
     path("vocabularies/", VocabularyListView.as_view(), name='vocabulary-list'),
     path("vocabularies/<uuid:pk>", VocabularyDetailsView.as_view(), name='vocabulary-detail'),
 
-    path("pairs/",PairsListView.as_view(), name='pairs'),
-    path("pairs/<uuid:pk>", PairsDetailsView.as_view(), name='pairs-uuid'),
+    path("pair/",LeftPairsListView.as_view(), name='pairs'),
+    path("pair/<uuid:pk>", LeftPairsDetailsView.as_view(), name='pairs-uuid'),
 
     path("match-pairs/", MatchPairsListView.as_view(), name='match-pairs'),
     path("match-pairs/<uuid:pk>", MatchPairsDetailsView.as_view(), name='match-pairs'),
