104e0bde67d3fc605eeb170d7be35efa61f62f61
shokh_smee <shohjahonobruyev3@gmail.com>
2025-04-23 18:37:11 +0500

    attendance bulk create

--- config/data/student/attendance/urls.py
+++ config/data/student/attendance/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('student/<uuid:pk>/', AttendanceListView.as_view(), name='student-attendance-list'),
     path('lesson/<uuid:pk>/', LessonAttendanceList.as_view(), name='lesson-list'),
 
+    path("bulk/",AttendanceBulkList.as_view(), name='bulk-attendance-list'),
+
     # path('filter/',FilterAttendanceView.as_view(), name='filter-attendance'),
 ]
