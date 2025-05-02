c6233594ed17289558cc1ea7b54203e6eaa680fc
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-02 20:05:56 +0500

    attendance bulk create

--- config/data/lid/new_lid/webhook.py
+++ config/data/lid/new_lid/webhook.py
@@ -5,28 +5,31 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from data.department.marketing_channel.models import MarketingChannel
 from data.lid.new_lid.models import Lid
 
 
-class WebhookView(APIView):
+class LidWebhook(APIView):
     def post(self, request, *args, **kwargs):
-        secret = request.headers.get("X-Webhook-Secret")
-        expected_secret = os.getenv("WEBHOOK_SECRET")
-
-        if secret != expected_secret:
-            return HttpResponseForbidden("Invalid webhook secret")
-
         data = request.data
-        if data:
-            lid = Lid.objects.create(
-                phone=data["phone"] or "",
-                first_name=data["first_name"] or "",
-                marketing_channel=data["marketing_channel"] or "",
-                lid_stage_type="NEW_LID",
-                lid_stages="YANGI_LEAD"
-            )
-            if lid:
-                return Response({"id": lid.id},
-                                status=status.HTTP_200_OK)
-
-        return Response({"message": "Webhook received"}, status=status.HTTP_200_OK)
+        if not data:
+            return Response({'error': 'No data provided'}, status=status.HTTP_400_BAD_REQUEST)
+        marketing_channel_id = MarketingChannel.objects.filter(name="Olimpiadalar").first().id
+        lid = Lid.objects.create(
+            first_name=data.get('first_name'),
+            last_name=data('last_name'),
+            marketing_channel_id=marketing_channel_id,
+            photo=None,
+            language_choise="UZB",
+            subject=None,
+            filial=None,
+            lid_stage_type="NEW_LID",
+            lid_stage="YANGI_LEAD",
+            call_operator=None,
+            service_manager=None,
+            sales_manager=None,
+            file=None,
+            student=None
+        )
+        if lid:
+            return Response({'status': 'Thank youu mann!'}, status=status.HTTP_201_CREATED)
