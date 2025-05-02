078531130eed3068dd0cb2a55a5d7ba07739e8bd
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-02 18:28:58 +0500

    attendance bulk create

--- config/data/student/studentgroup/views.py
+++ config/data/student/studentgroup/views.py
@@ -173,23 +173,21 @@ class SecondaryGroupUpdate(UpdateAPIView):
     serializer_class = SecondaryStudentsGroupSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_object(self):
-        queryset = self.get_queryset()
-
-        lid_id = self.request.query_params.get('lid_id')
-        student_id = self.request.query_params.get('student_id')
-
-        group_id = self.request.query_params.get('group_id')
-
-        if student_id and group_id:
-            obj = get_object_or_404(queryset, student__id=student_id, group__id=group_id)
-        elif lid_id:
-            obj = get_object_or_404(queryset, lid__id=lid_id)
-        else:
-            raise ValidationError("You must provide either 'lid_id' or both 'student_id' and 'group_id'.")
-
-        self.check_object_permissions(self.request, obj)
-        return obj
+    # def get_object(self):
+    #     queryset = self.get_queryset()
+    #
+    #     lid_id = self.request.query_params.get('lid_id')
+    #     student_id = self.request.query_params.get('student_id')
+    #
+    #     if lid_id:
+    #         obj = get_object_or_404(queryset, lid__id=lid_id)
+    #     elif student_id:
+    #         obj = get_object_or_404(queryset, student__id=student_id)
+    #     else:
+    #         raise ValidationError("You must provide either 'lid_id' or 'student_id'.")
+    #
+    #     self.check_object_permissions(self.request, obj)
+    #     return obj
 
 
 class StudentGroupDelete(APIView):
