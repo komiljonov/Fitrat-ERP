43a328e3a60134f8fcce4e57dd5821f7674fdd93
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-18 19:23:52 +0500

    optimization

--- config/data/finances/timetracker/models.py
+++ config/data/finances/timetracker/models.py
@@ -23,3 +23,19 @@ class Employee_attendance(BaseModel):
         return f"{self.user.full_name}   {self.action}   {self.type}"
 
 
+class UserTimeLine(BaseModel):
+    user : "CustomUser" = models.ForeignKey("account.CustomUser", on_delete=models.CASCADE, related_name="user_timeline")
+    day = models.CharField(choices=[
+        ("Monday","Monday"),
+        ("Tuesday","Tuesday"),
+        ("Wednesday","Wednesday"),
+        ("Thursday","Thursday"),
+        ("Friday","Friday"),
+        ("Saturday","Saturday"),
+        ("Sunday","Sunday"),
+    ],max_length=120, null=True, blank=True)
+    start_time = models.TimeField(default=timezone.now)
+    end_time = models.TimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.full_name}   {self.day}   {self.start_time}   {self.end_time}"
