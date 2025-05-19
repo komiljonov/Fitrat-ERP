ef880b964eda341940fd32d02c7937dede3ac564
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-19 17:17:12 +0500

    optimization

--- config/data/student/mastering/apps.py
+++ config/data/student/mastering/apps.py
@@ -5,5 +5,5 @@ class MasteringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.student.mastering'
 
-    def ready(self):
-         import data.student.mastering.signals
+    # def ready(self):
+    #      import data.student.mastering.signals
