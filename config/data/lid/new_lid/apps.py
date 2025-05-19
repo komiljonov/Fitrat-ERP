6fce51af9778e5235ad873fac569d97ce71b158c
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-19 17:11:13 +0500

    optimization

--- config/data/lid/new_lid/apps.py
+++ config/data/lid/new_lid/apps.py
@@ -7,5 +7,5 @@ class NewLidConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.lid.new_lid'
 
-    def ready(self):
-        import data.lid.new_lid.signals
+    # def ready(self):
+    #     import data.lid.new_lid.signals
