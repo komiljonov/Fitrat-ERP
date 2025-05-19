798451cafb32dd374dab27863386c96a33506f43
shokh_smee <shohjahonobruyev3@gmail.com>
2025-05-19 17:17:12 +0500

    optimization

--- config/data/finances/compensation/apps.py
+++ config/data/finances/compensation/apps.py
@@ -5,5 +5,5 @@ class CompensationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.finances.compensation'
 
-    def ready(self):
-        import data.finances.compensation.signals
+    # def ready(self):
+    #     import data.finances.compensation.signals
