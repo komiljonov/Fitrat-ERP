bce29ef93d0a0b033be7ede8bb8342d30cf7638e
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-03 12:01:23 +0500

    results filters

--- config/data/finances/timetracker/serializers.py
+++ config/data/finances/timetracker/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from icecream import ic
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -6,6 +7,7 @@ from data.account.models import CustomUser
 
 from .models import Employee_attendance, UserTimeLine, Stuff_Attendance
 from ...account.serializers import UserSerializer
+from ...student.groups.models import Group, Day
 
 
 class Stuff_AttendanceSerializer(serializers.ModelSerializer):
@@ -25,8 +27,18 @@ class Stuff_AttendanceSerializer(serializers.ModelSerializer):
 
 
 class TimeTrackerSerializer(serializers.ModelSerializer):
-    attendance = serializers.PrimaryKeyRelatedField(queryset=Employee_attendance.objects.all(),many=True,allow_null=True)
-    employee = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(),allow_null=True)
+    attendance = serializers.PrimaryKeyRelatedField(
+        queryset=Employee_attendance.objects.all(), many=True, allow_null=True
+    )
+    employee = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.all(), allow_null=True
+    )
+    group = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all(), allow_null=True
+    )
+
+    groups = serializers.SerializerMethodField()  # <-- Bu yangi qo‘shildi
+
     class Meta:
         model = Employee_attendance
         fields = [
@@ -35,9 +47,48 @@ class TimeTrackerSerializer(serializers.ModelSerializer):
             "attendance",
             "date",
             "amount",
+            "group",
             "status",
             "created_at",
+            "groups",  # <-- Bu yerga ham qo‘shildi
         ]
+
+    def get_groups(self, obj):
+        DAY_TRANSLATIONS = {
+            'MONDAY': 'Dushanba',
+            'TUESDAY': 'Seshanba',
+            'WEDNESDAY': 'Chorshanba',
+            'THURSDAY': 'Payshanba',
+            'FRIDAY': 'Juma',
+            'SATURDAY': 'Shanba',
+            'SUNDAY': 'Yakshanba',
+        }
+
+        user = CustomUser.objects.filter(id=obj.employee.id).first()
+        result = []
+
+        if user and user.role in ["TEACHER", "ASSISTANT"]:
+            groups = Group.objects.filter(Q(teacher=user) | Q(secondary_teacher=user)).distinct()
+            for group in groups:
+                days_qs = group.scheduled_day_type.all()
+                days_uz = [DAY_TRANSLATIONS.get(day.name.upper(), day.name) for day in days_qs]
+
+                result.append({
+                    "group_name": group.name,
+                    "started_at": group.started_at.strftime('%H:%M'),
+                    "ended_at": group.ended_at.strftime('%H:%M'),
+                    "days": days_uz
+                })
+        else:
+            timeline = UserTimeLine.objects.filter(user=obj.employee).all()
+            for i in timeline:
+                result.append({
+                    "started_at": i.start_time.strftime('%H:%M'),
+                    "ended_at": i.end_time.strftime('%H:%M'),
+                    "days": i.day  # if this is a string like 'MONDAY', you can also translate it
+                })
+
+        return result
 
     def create(self, validated_data):
         if validated_data["attendance"] is None:
