519cc760cca56982644f1ea9a9e35dbe09524a29
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-05 13:54:25 +0500

    lid statistics

--- config/data/finances/timetracker/views.py
+++ config/data/finances/timetracker/views.py
@@ -45,6 +45,7 @@ class TimeTrackerList(ListCreateAPIView):
         if employee:
             queryset = queryset.filter(employee__id=employee)
         if status:
+            ic(status)
             queryset = queryset.filter(status=status)
         if date:
             queryset = queryset.filter(date=parse_datetime(date))
