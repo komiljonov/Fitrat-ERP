3627ae66185716b36463bc84a80a41d611711340
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-10 13:58:18 +0500

    lid statistics

--- config/data/student/quiz/serializers.py
+++ config/data/student/quiz/serializers.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 from icecream import ic
-from lxml.saxparser import comment
 from rest_framework import serializers, status
 from rest_framework.response import Response
 
