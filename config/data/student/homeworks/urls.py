d6c3306dadf88a95263cc93d6137b24966d16ca7
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-12 14:00:37 +0500

    attendance update recalculate

--- config/data/student/homeworks/urls.py
+++ config/data/student/homeworks/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 
 from data.student.homeworks.views import HomeworkListCreateView, HomeworkDetailView, HomeworkHistoryListCreateView, \
-    HomeworkHistoryView
+    HomeworkHistoryView, HomeworkListNoPgCreateView
 
 urlpatterns = [
     path("",HomeworkListCreateView.as_view()),
     path("<uuid:pk>/",HomeworkDetailView.as_view()),
+    path("no-pg",HomeworkListNoPgCreateView.as_view()),
+
 
     path("history/", HomeworkHistoryListCreateView.as_view()),
     path("history/<uuid:pk>",HomeworkHistoryView.as_view()),
