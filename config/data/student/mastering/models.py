167837c62681f51ca571ee1574dca92398b30bb8
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-12 14:07:41 +0500

    attendance update recalculate

--- config/data/student/mastering/models.py
+++ config/data/student/mastering/models.py
@@ -35,3 +35,37 @@ class MasteringTeachers(BaseModel):
         return f"{self.teacher.first_name} {self.ball}"
 
 
+# class StudentQuiz(BaseModel):
+#     quiz : "Quiz" = models.ForeignKey(Quiz, on_delete=models.SET_NULL, null=True,blank=True,
+#                                       related_name='student_quiz')
+#
+#     standard_quiz = models.ForeignKey("quiz.Question", on_delete=models.SET_NULL, null=True,blank=True,
+#                                       related_name='student_quiz_standard')
+#     fill_gap_quiz = models.ForeignKey("quiz.Fill_gaps", on_delete=models.SET_NULL, null=True,blank=True,
+#                                       related_name='student_quiz_fill_gaps')
+#     vocab_quiz = models.ForeignKey("quiz.Vocabulary", on_delete=models.SET_NULL, null=True,blank=True,
+#                                    related_name='student_quiz_vocab')
+#     match_quiz = models.ForeignKey("quiz.MatchPairs", on_delete=models.SET_NULL, null=True,blank=True,
+#                                    related_name='student_quiz_match_pairs')
+#     objective_quiz = models.ForeignKey("quiz.ObjectiveTest", on_delete=models.SET_NULL, null=True,blank=True,
+#                                        related_name='student_quiz_objective_test')
+#     cloze_quiz = models.ForeignKey("quiz.Cloze_Test", on_delete=models.SET_NULL, null=True,blank=True,
+#                                    related_name='student_quiz_cloze')
+#     image_quiz = models.ForeignKey("quiz.Image", on_delete=models.SET_NULL, null=True,blank=True,
+#                                    related_name='student_quiz_image')
+#     true_false_quiz = models.ForeignKey("quiz.TrueFalse", on_delete=models.SET_NULL, null=True,blank=True,
+#                                         related_name='student_quiz_true_false')
+#
+#     time = models.IntegerField(default=0)
+#
+#     answer = models.ForeignKey("quiz.Answer", on_delete=models.SET_NULL, null=True,blank=True,
+#                                related_name='student_quiz_answer')
+#
+#     is_correct = models.BooleanField(default=False)
+#
+#     student = models.ForeignKey('student.Student', on_delete=models.SET_NULL, null=True,blank=True,
+#                                 related_name='student_quiz_student')
+#
+#     def __str__(self):
+#
+#         return f"{self.student.first_name} {self.student.last_name}"
