2c4e801f4dcbe5a9dc39e319435012f453d26801
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-17 13:38:07 +0500

    attendance update recalculate

--- config/data/exam_results/apps.py
+++ config/data/exam_results/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExamResultsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'exam_results'
+    name = 'data.exam_results'
