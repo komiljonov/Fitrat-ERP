449074fd921fc8777c7b9e5fb37d8471f78f813c
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-18 19:13:59 +0500

    z

--- config/data/student/subject/admin.py
+++ config/data/student/subject/admin.py
@@ -7,6 +7,6 @@ from .models import Theme
 class ThemeAdmin(admin.ModelAdmin):
     list_display = ('subject__name','title',"course__name","level")
     search_fields = ('subject','title',"course__name","level")
-    list_filter = ('subject','title',"course__name","level")
+    list_filter = ('subject','title',"course","level")
 
 
