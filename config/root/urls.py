d8d12157983bc119384f4d0f717a0daa8aac6e80
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-23 10:30:26 +0500

    quiz results create

--- config/root/urls.py
+++ config/root/urls.py
@@ -80,6 +80,8 @@ urlpatterns = [
     path('upload/', include('data.upload.urls')),
     path('dashboard/', include('data.dashboard.urls')),
 
+    path("quiz-results/", include("data.quiz_results.urls")),
+
     path("docs<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui", ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
