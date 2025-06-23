11805522d9a4116cbbe3fb67b2685087241c7e91
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-23 17:37:26 +0500

    quiz results create

--- config/data/finances/timetracker/views.py
+++ config/data/finances/timetracker/views.py
@@ -118,9 +118,13 @@ class AttendanceList(ListCreateAPIView):
                     )
             elif check_in and actions and check_out:
 
+                print(actions)
+
                 sorted_actions = sorted(actions, key=lambda x: x['start'])
+
+                print(sorted_actions)
                 sorted_actions = sorted_actions[0]
-                ic(sorted_actions)
+
 
                 att = Stuff_Attendance.objects.filter(
                     employee__id=employee,
