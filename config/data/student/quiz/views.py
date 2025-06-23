84ee169a6c597868ab2598514e73205deb926990
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-23 18:35:34 +0500

    quiz results create

--- config/data/student/quiz/views.py
+++ config/data/student/quiz/views.py
@@ -284,7 +284,7 @@ class QuizCheckAPIView(APIView):
             user_answer_id = user_answer.get("answer", "")
             is_correct = str(user_answer_id) == str(correct_answer_id)
 
-            print(question.get("file", {}))
+            print(question.get("question", {}))
 
             context = {
                 'request': self.request,
