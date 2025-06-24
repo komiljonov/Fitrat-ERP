3258d8cf82fc907e46ae88dc53621ee7b4221878
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-24 18:53:15 +0500

    quiz results create

--- config/data/student/quiz/views.py
+++ config/data/student/quiz/views.py
@@ -92,15 +92,21 @@ class QuizCheckAPIView(APIView):
                 results["summary"]["wrong_count"] += 1
 
             results["details"][qtype].append(result_data)
+
         context = {
             'request': request,
             'user': request.user,
             'custom_data': 'some_value'
         }
+
         existing_results = QuizResult.objects.filter(quiz=quiz, student=student).first()
         existing_data = QuizResultSerializer(existing_results,context=context).data if existing_results else None
 
-        data_length = len(existing_data)
+        print([v for k,v in existing_data.items()])
+
+        data_length = {
+            len(v) for k, v in existing_data.items()
+        }
 
 
         RESULT_FIELDS_MAP = {
