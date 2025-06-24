227f52a13e544bdc0f99cd92f8188c2a53a44dc2
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-24 18:58:33 +0500

    quiz results create

--- config/data/exam_results/urls.py
+++ config/data/exam_results/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
 
-from data.exam_results.views import QuizRestAPIView
+from data.exam_results.views import QuizRestAPIView, UnitTestListCreateAPIView
 
 urlpatterns = [
     path('', QuizRestAPIView.as_view()),
+
+    path("unit/",UnitTestListCreateAPIView.as_view()),
+    path("unit/<uuid:pk>",QuizRestAPIView.as_view()),
 ]
