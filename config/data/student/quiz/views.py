d20db80053a41a0f8bc139bdebef52539e01265d
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-24 17:18:46 +0500

    quiz results create

--- config/data/student/quiz/views.py
+++ config/data/student/quiz/views.py
@@ -100,8 +100,6 @@ class QuizCheckAPIView(APIView):
         existing_results = QuizResult.objects.filter(quiz=quiz, student=student).first()
         existing_data = QuizResultSerializer(existing_results,context=context).data if existing_results else None
 
-        print(existing_data)
-
         data_length = len(existing_data)
 
 
