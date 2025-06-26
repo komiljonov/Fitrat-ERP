175f541972ec475c8b72a4a8d582c153623f74f0
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-26 16:13:52 +0500

    quiz results create

--- config/data/lid/archived/signals.py
+++ config/data/lid/archived/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Student, Archived
+from ...finances.compensation.models import Comments
 from ...notifications.models import Notification
 
 
@@ -12,7 +13,23 @@ def on_create(sender, instance: Archived, created, **kwargs):
             instance.student.is_archived = True
             instance.student.save()
 
+            comment = Comments.objects.create(
+                asos=None,
+                creator=instance.creator,
+                user=instance.student,
+                monitoring=None,
+                comment=f"Arxivlandi {instance.created_at} sanasida, sabab: {instance.reason}",
+            )
+
         if instance.lid:
             instance.lid.is_archived = True
             instance.lid.save()
+            comment = Comments.objects.create(
+                asos=None,
+                creator=instance.creator,
+                user=instance.lid,
+                monitoring=None,
+                comment=f"Arxivlandi {instance.created_at} sanasida, sabab: {instance.reason}",
+            )
+
 
