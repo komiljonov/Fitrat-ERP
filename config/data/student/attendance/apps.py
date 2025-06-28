affce3dcb093dfad3d31e60243865571a54d5a20
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-28 13:50:55 +0500

    commented appps

--- config/data/student/attendance/apps.py
+++ config/data/student/attendance/apps.py
@@ -5,5 +5,5 @@ class AttendanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.student.attendance'
 
-    # def ready(self):
-    #     import data.student.attendance.signals
+    def ready(self):
+        import data.student.attendance.signals
