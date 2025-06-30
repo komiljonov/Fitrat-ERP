1c76f701367a7af307eb044ae6551087ae64b98f
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-30 12:11:12 +0500

    ARCHIVED UPDATE

--- config/data/lid/archived/models.py
+++ config/data/lid/archived/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from ..new_lid.models import Lid
 from ...command.models import BaseModel
 from ...student.student.models import Student
+from ...comments.views import Comment
+
 
 from ...account.models import CustomUser
 
@@ -14,8 +16,12 @@ class Archived(BaseModel):
     lid : "Lid" = models.ForeignKey('new_lid.Lid', on_delete=models.SET_NULL,
                                     null=True, blank=True, related_name='archived_lead')
     student : "Student" = models.ForeignKey('student.Student', on_delete=models.SET_NULL,
-                                            null=True, blank=True, related_name='archived_students')
+                                    null=True, blank=True, related_name='archived_students')
     reason = models.TextField()
+
+    comment : "Comment" = models.ForeignKey('comments.Comment',
+                                    on_delete=models.SET_NULL,null=True,blank=True,related_name='archived_comments')
+    is_archived = models.BooleanField(default=False)
 
     def __str__(self):
         return (self.lid.first_name if self.lid else self.student.first_name)+ " " + (self.lid.last_name if self.lid else self.student.last_name)
