469f6448124b32009313167a7392ccefd1dbd3a5
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-04 14:10:30 +0500

    ARCHIVED UPDATE

---
+++ config/data/event/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import Event
+from ..upload.models import File
+from ..upload.serializers import FileUploadSerializer
+
+
+class EventSerializer(serializers.ModelSerializer):
+
+    file = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),many=True,allow_null=True)
+    photo = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),allow_null=True)
+
+    class Meta:
+        model = Event
+        fields = [
+            "id",
+            "file",
+            "photo",
+            "link_preview",
+            "link",
+            "comment",
+            "end_date",
+            "status",
+            "created_at",
+        ]
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+
+        rep["file"] = FileUploadSerializer(instance.file,many=True,context=self.context).data
+        rep["photo"] = FileUploadSerializer(instance.photo,many=True,context=self.context).data
+        return rep
