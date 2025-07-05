e2226a7b9f6dd794e7d58e55f500e387b7ff7a41
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-05 13:03:10 +0500

    ARCHIVED UPDATE

--- config/data/library/models.py
+++ config/data/library/models.py
@@ -11,17 +11,17 @@ class LibraryCategory(BaseModel):
         return self.name
 
 
-class Library(BaseModel):
-    category : "LibraryCategory" = models.ForeignKey("library.LibraryCategory", on_delete=models.SET_NULL, null=True,blank=True)
-    name = models.TextField()
-    choice = models.CharField(choices=[
-        ("withAudio", "With Audio"),
-        ("noAudio", "No Audio"),
-    ],max_length=100,null=True,blank=True)
-
-    book : "File" = models.ForeignKey("upload.File", on_delete=models.SET_NULL, null=True,blank=True, related_name="books_files")
-
-    file : "File" = models.ManyToManyField("upload.File",related_name="libraries_files")
-    def __str__(self):
-        return self.name
+# class Library(BaseModel):
+#     category : "LibraryCategory" = models.ForeignKey("library.LibraryCategory", on_delete=models.SET_NULL, null=True,blank=True)
+#     name = models.TextField()
+#     choice = models.CharField(choices=[
+#         ("withAudio", "With Audio"),
+#         ("noAudio", "No Audio"),
+#     ],max_length=100,null=True,blank=True)
+#
+#     book : "File" = models.ForeignKey("upload.File", on_delete=models.SET_NULL, null=True,blank=True, related_name="books_files")
+#
+#     file : "File" = models.ManyToManyField("upload.File",related_name="libraries_files")
+#     def __str__(self):
+#         return self.name
 
