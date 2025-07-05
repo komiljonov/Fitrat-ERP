83128a4ffe9ff404bb661c891d0a3c8aa1eb029d
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-05 13:06:23 +0500

    ARCHIVED UPDATE

--- config/data/library/urls.py
+++ config/data/library/urls.py
@@ -3,11 +3,11 @@
 from django.urls import include, path
 from django.conf.urls.static import static
 
-from .views import CategoryCreateAPIView,CategoryRetrieveUpdateDestroyAPIView,ListCreateAPIView,LibraryRetrieveUpdateDestroyAPIView
-
+from .views import CategoryCreateAPIView, CategoryRetrieveUpdateDestroyAPIView, LibraryRetrieveUpdateDestroyAPIView, \
+    LibraryCreateAPIView
 
 urlpatterns = [
-    path("",ListCreateAPIView.as_view()),
+    path("",LibraryCreateAPIView.as_view()),
     path("<uuid:pk>/",CategoryRetrieveUpdateDestroyAPIView.as_view()),
 
     path("category/",CategoryCreateAPIView.as_view()),
