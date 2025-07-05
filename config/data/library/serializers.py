4b26f40167eed4a5328f62992cf0ce0619901ead
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-05 13:05:05 +0500

    ARCHIVED UPDATE

--- config/data/library/serializers.py
+++ config/data/library/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import LibraryCategory,Library
+from .models import LibraryCategory
 from ..upload.models import File
 from ..upload.serializers import FileUploadSerializer
 
@@ -15,23 +15,23 @@ class LibraryCategorySerializer(serializers.ModelSerializer):
         ]
 
 
-class LibrarySerializer(serializers.ModelSerializer):
-    book = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),allow_null=False)
-    file = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),many=True,allow_null=False)
-    class Meta:
-        model = Library
-        fields = [
-            "id",
-            "category",
-            "name",
-            "choice",
-            "book",
-            "file",
-            "created_at",
-        ]
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep["book"] = FileUploadSerializer(instance.book,context=self.context).data
-        rep["file"] = FileUploadSerializer(instance.file,many=True,context=self.context).data
-
-        return rep
+# class LibrarySerializer(serializers.ModelSerializer):
+#     book = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),allow_null=False)
+#     file = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(),many=True,allow_null=False)
+#     class Meta:
+#         model = Library
+#         fields = [
+#             "id",
+#             "category",
+#             "name",
+#             "choice",
+#             "book",
+#             "file",
+#             "created_at",
+#         ]
+#     def to_representation(self, instance):
+#         rep = super().to_representation(instance)
+#         rep["book"] = FileUploadSerializer(instance.book,context=self.context).data
+#         rep["file"] = FileUploadSerializer(instance.file,many=True,context=self.context).data
+#
+#         return rep
