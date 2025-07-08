d82968fb0e8ed40df2fcc98f33b414a674a3b6b1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-08 12:18:13 +0500

    ARCHIVED UPDATE

--- config/data/student/shop/admin.py
+++ config/data/student/shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from data.student.shop.models import Coins
+from data.student.student.models import Student
+
+
 # Register your models here.
+@admin.register(Coins)
+class CoinsAdmin(admin.ModelAdmin):
+    list_display = ('student__first_name', 'status', 'coin')
+    search_fields = ('student__first_name',)
+    list_filter = ('status',)
