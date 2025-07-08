03abf387350c2cd6dc892ffa717864a46c54c1d1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-08 14:55:47 +0500

    ARCHIVED UPDATE

--- config/data/student/shop/utils.py
+++ config/data/student/shop/utils.py
@@ -1,22 +1,10 @@
-# from decimal import Decimal
-#
-#
-# from .models import Coins, CoinsSettings
-#
-# def give_coin(type,choice, student,point,):
-#
-#     if choice == "Speaking":
-#         coin = CoinsSettings.objects.filter(
-#             choice=choice,
-#             type=type,
-#             from_point=from_point,
-#             to_point=to_point,
-#         ).first()
-#         if coin:
-#             Coins.objects.create(
-#                 student=student,
-#                 choice=choice,
-#                 status="Given",
-#                 coin=coin.coin,
-#                 comment=f"Sizning {choice} topshirig'idan {from_point}"
-#             )
+from decimal import Decimal
+from .models import Coins, CoinsSettings
+
+def give_coin(type,choice, student,from_point, to_point: None,):
+
+    if type == "Single":
+        pass
+
+    elif type == "Double":
+        pass
