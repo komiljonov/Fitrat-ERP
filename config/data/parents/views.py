6e7f4b5787257e55ca6f21d837ab45eb404f4a13
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-08 13:21:11 +0500

    ARCHIVED UPDATE

--- config/data/parents/views.py
+++ config/data/parents/views.py
@@ -1,7 +1,9 @@
 from django.db.models import Q
+from rest_framework import status
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Relatives
 from .serializers import RelativesSerializer
@@ -41,3 +43,31 @@ class StudentsRelativesListView(ListAPIView):
         id = self.kwargs.get('pk')
         queryset = Relatives.objects.filter(Q(student__id=id) | Q(lid__id=id))
         return queryset
+
+
+class ParentsStudentsAPIView(APIView):
+    queryset = Relatives.objects.all()
+
+    def get(self, request):
+
+        phone = request.GET.get('phone')
+        id = self.kwargs.get('pk')
+
+        students = Relatives.objects.all()
+
+        if phone:
+            students = students.filter(phone=phone)
+        if id:
+            students = students.filter(id=id)
+
+        if students:
+            students_data = []
+
+            for student in students:
+                students_data.append({
+                    'id': student.student.id,
+                    'full_name': f"{student.student.first_name} {student.student.last_name}",
+                    "phone" : student.student.phone,
+                    "balance": student.student.balance,
+                })
+            return Response(students_data, status=status.HTTP_200_OK)
