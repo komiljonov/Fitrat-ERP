dce0ec342dbf4dec7143ae2b6470443817f6af59
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-08 13:23:30 +0500

    ARCHIVED UPDATE

--- config/data/parents/urls.py
+++ config/data/parents/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ParentListView, ParentDetailView,RelativesListNoPGView, StudentsRelativesListView
+from .views import ParentListView, ParentDetailView, RelativesListNoPGView, StudentsRelativesListView, \
+    ParentsStudentsAPIView
 
 urlpatterns = [
     path('', ParentListView.as_view()),
     path('<uuid:pk>', ParentDetailView.as_view()),
     path('no-pg/', RelativesListNoPGView.as_view()),
     path('student/<uuid:pk>', StudentsRelativesListView.as_view()),
+
+    path("students/",ParentsStudentsAPIView.as_view())
 ]
