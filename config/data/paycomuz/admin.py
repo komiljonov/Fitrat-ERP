798de3e7e458800de579c796df7390a6db380295
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-09 18:23:36 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/admin.py
+++ config/data/paycomuz/admin.py
@@ -6,10 +6,10 @@ from .models import Transaction
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('id', '_id', 'request_id', 'amount', 'state', 'status',)
+    list_display = ('id', 'request_id', 'amount', 'state', 'status',)
     list_display_links = ('id',)
     list_filter = ('status',)
-    search_fields = ['request_id', 'status', 'id', '_id']
+    search_fields = ['request_id', 'status', 'id',]
 
 
 admin.site.register(Transaction, TransactionAdmin)
