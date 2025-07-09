1814cc97a84123732ead567a99a453ea481028e4
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-09 17:18:40 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/serializers/serializers.py
+++ config/data/paycomuz/serializers/serializers.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 from decimal import Decimal
 
 class GeneratePaymentLinkSerializer(serializers.Serializer):
-    order_id = serializers.CharField()
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    return_url = serializers.URLField()
+    order_id = serializers.CharField(required=False, allow_null=True)
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2,required=False, allow_null=True)
+    return_url = serializers.URLField(required=False, allow_null=True)
