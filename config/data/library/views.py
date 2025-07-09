d32310925bd61e1b7e824c8d82a53c7e9e805138
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-09 12:40:24 +0500

    ARCHIVED UPDATE

--- config/data/library/views.py
+++ config/data/library/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from unicodedata import category
+
+from .models import LibraryCategory, Library
+from .serializers import LibraryCategorySerializer, LibrarySerializer
+
 
 # Create your views here.
-
-from .serializers import LibraryCategorySerializer,LibrarySerializer
-from .models import LibraryCategory,Library
-
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 class CategoryCreateAPIView(ListCreateAPIView):
     queryset = LibraryCategory.objects.all()
     serializer_class = LibraryCategorySerializer
 
     pagination_class = None
-
 
     def get_paginated_response(self, data):
         return Response({data})
@@ -36,12 +34,13 @@ class LibraryCreateAPIView(ListCreateAPIView):
         search = self.request.GET.get('search')
 
         if search:
-            queryset = queryset.filter(name__icontains=search)
+            queryset = queryset.filter(Q(name__icontains=search) | Q(title__icontains=search))
 
         if category:
             queryset = queryset.filter(category__id=category)
 
         return queryset
+
 
 class LibraryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Library.objects.all()
