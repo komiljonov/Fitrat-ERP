45080fe72155b91714d1c9a7b8a8869c30587f41
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-09 18:44:34 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/models.py
+++ config/data/paycomuz/models.py
@@ -13,14 +13,15 @@ class Transaction(models.Model):
         (CANCELED, 'canceled')
     )
 
+    _id = models.CharField(max_length=255)
     request_id = models.IntegerField()
     order_key = models.CharField(max_length=255, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.IntegerField(blank=True, null=True)
     status = models.CharField(choices=STATUS, default='processing', max_length=55)
-    perform_datetime = models.CharField(null=True,blank=True, max_length=255)
-    cancel_datetime = models.CharField(null=True,blank=True, max_length=255)
-    created_datetime = models.CharField(null=True,blank=True, max_length=255)
+    perform_datetime = models.CharField(null=True, max_length=255)
+    cancel_datetime = models.CharField(null=True, max_length=255)
+    created_datetime = models.CharField(null=True, max_length=255)
     reason = models.IntegerField(null=True)
 
     def __str__(self):
