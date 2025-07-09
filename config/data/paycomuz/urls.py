33957b9b01930288a74ee2ea39601c9057dfcbcf
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-09 16:47:40 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/urls.py
+++ config/data/paycomuz/urls.py
@@ -1,7 +1,9 @@
-from django.urls import include, path
-from .views import PaycomWebhookView
+# payments/urls.py
 
+from django.urls import path
+from .views import GeneratePaymeURLView, PaycomWebhookView
 
 urlpatterns = [
-    path('transaction/', PaycomWebhookView.as_view(), name='paycom-webhook'),
+    path("paycom/", PaycomWebhookView.as_view(), name="paycom-webhook"),
+    path("generate-payment/", GeneratePaymeURLView.as_view(), name="generate-payment-link"),
 ]
