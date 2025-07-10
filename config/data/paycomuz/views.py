f748fb2987b1721dbed1d1ee31221f71657a15a2
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-10 17:14:09 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/views.py
+++ config/data/paycomuz/views.py
@@ -334,7 +334,7 @@ class GeneratePaymeURLView(APIView):
         return Response({'payment_url': url}, status=status.HTTP_200_OK)
 
 
-class PaycomWebhookView(PayComResponse):
+class PaycomWebhookView(MerchantAPIView):
     VALIDATE_CLASS = CheckOrder
 
 
