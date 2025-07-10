ade6b2be643030c92a6b211746b2566f2fe1a165
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-10 17:05:42 +0500

    ARCHIVED UPDATE

--- config/data/paycomuz/__init__.py
+++ config/data/paycomuz/__init__.py
@@ -11,22 +11,7 @@ class Paycom(PayComResponse):
         >>> self.check_order(amount=amount, account=account)
         """
 
-        from ..lid.new_lid.models import Lid
-        from ..student.student.models import Student
-
-        order_key = account.get("order_id")  # Must match `order_key` in Transaction
-
-        if not order_key:
-            return self.ORDER_NOT_FOUND
-
-        # Check if order exists in Student or Lid
-        student_exists = Student.objects.filter(id=order_key).exists()
-        lead_exists = Lid.objects.filter(id=order_key).exists()
-
-        if not (student_exists or lead_exists):
-            return self.ORDER_NOT_FOUND
-
-        return self.ORDER_FOUND
+        pass
 
     def successfully_payment(self, account, transaction, *args, **kwargs):
         """
