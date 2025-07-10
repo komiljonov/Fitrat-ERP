39465ca43929d57f92d3ee4562bbd5cd82eac30f
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-10 10:04:07 +0500

    ARCHIVED UPDATE

--- config/data/lid/archived/models.py
+++ config/data/lid/archived/models.py
@@ -26,3 +26,15 @@ class Archived(BaseModel):
     def __str__(self):
         return (self.lid.first_name if self.lid else self.student.first_name)+ " " + (self.lid.last_name if self.lid else self.student.last_name)
 
+
+
+class Frozen(BaseModel):
+    creator : "CustomUser" = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE,related_name="frozen_user_creator")
+    lid : "Lid" = models.ForeignKey('new_lid.Lid', on_delete=models.SET_NULL,null=True,blank=True, related_name='frozen_lead')
+    student : "Student" = models.ForeignKey('student.Student', on_delete=models.SET_NULL,null=True,blank=True,related_name='frozen_students')
+    reason = models.TextField()
+    comment : "Comment" = models.ForeignKey('comments.Comment',on_delete=models.SET_NULL,null=True,blank=True,related_name='frozen_comments')
+    is_frozen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (self.lid.first_name if self.lid else self.student.first_name)+ " " + (self.lid.last_name if self.lid else self.student.last_name)
