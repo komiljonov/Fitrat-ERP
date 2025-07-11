3780f7e188139cc8de09da6d596a504875c0ed20
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-11 16:12:05 +0500

    ARCHIVED UPDATE

--- config/data/notifications/views.py
+++ config/data/notifications/views.py
@@ -1,6 +1,8 @@
+from rest_framework import status
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -39,3 +41,17 @@ class NotificationListNoPG(ListAPIView):
 
     def get_paginated_response(self, data):
         return Response(data)
+
+
+
+class MarkAllNotificationsReadAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        notifications = Notification.objects.filter(user=request.user, is_read=False)
+        updated_count = notifications.update(is_read=True)
+
+        return Response(
+            {"message": f"{updated_count} notifications marked as read."},
+            status=status.HTTP_200_OK
+        )
