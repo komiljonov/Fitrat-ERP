8e8a278082d92474e6e45deffd04efb6027e74b1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-11 19:46:38 +0500

    ARCHIVED UPDATE

--- config/root/settings.py
+++ config/root/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(seconds=20),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=50),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=10),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
