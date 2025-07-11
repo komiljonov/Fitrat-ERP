f57946dd08cac9286a1be2b59d5a96cfc7911e73
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-11 16:12:05 +0500

    ARCHIVED UPDATE

--- config/data/notifications/urls.py
+++ config/data/notifications/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import NotificationListAPIView,NotificationListNoPG,NotificationRetrieveUpdateDestroyAPIView
+from .views import NotificationListAPIView, NotificationListNoPG, NotificationRetrieveUpdateDestroyAPIView, \
+    MarkAllNotificationsReadAPIView
 
 urlpatterns = [
     path('',NotificationListAPIView.as_view()),
     path('<uuid:pk>',NotificationRetrieveUpdateDestroyAPIView.as_view()),
     path('no-pg/',NotificationListNoPG.as_view()),
+
+    path("notifications/mark-all-read/",MarkAllNotificationsReadAPIView.as_view()),
 ]
