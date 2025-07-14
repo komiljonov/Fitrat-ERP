03416c14b16ede4c8dc5631534ae6153be49a69d
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-14 12:15:44 +0500

    ARCHIVED UPDATE

--- config/data/account/admin.py
+++ config/data/account/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
-from ..account.models import CustomUser
 from ..account.forms import CustomUserChangeForm, CustomUserCreationForm
+from ..account.models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    form = CustomUserChangeForm,CustomUserCreationForm
+    form = CustomUserChangeForm, CustomUserCreationForm
     model = CustomUser
     list_display = ("phone", "is_staff", "is_active",)
     list_filter = ("phone", "is_staff", "is_active",)
@@ -22,18 +23,17 @@ class CustomUserAdmin(UserAdmin):
             "classes": ("wide",),
             "fields": (
                 "phone", "password1", "is_staff",
-                "is_active", "name", "phone","calculate_penalties"
+                "is_active", "name", "phone", "calculate_penalties"
 
             )}
-        ),
+         ),
     )
     search_fields = ("phone", "name",)
     ordering = ("phone",)
 
     change_form_template = 'admin/auth/user/user_change_form.html'
 
+
 admin.site.register(CustomUser)
-
-
-
-
+admin.site.unregister(OutstandingToken)
+admin.site.unregister(BlacklistedToken)
