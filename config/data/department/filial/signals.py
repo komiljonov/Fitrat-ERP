a943057c86da731a4abe1e244cabf38580cf4d2c
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-14 13:01:13 +0500

    ARCHIVED UPDATE

---
+++ config/data/department/filial/signals.py
@@ -0,0 +1,25 @@
+from data.department.filial.models import Filial
+from django.db.models.signals import m2m_changed, post_save
+from django.dispatch import receiver
+
+from data.account.models import CustomUser
+from data.command.models import UserFilial
+
+
+# Handle additions and removals
+@receiver(m2m_changed, sender=CustomUser.filial.through)
+def sync_user_filials(sender, instance: CustomUser, action, pk_set, **kwargs):
+    if action == 'post_add':
+        for filial_id in pk_set:
+            filial = Filial.objects.get(pk=filial_id)
+            UserFilial.objects.get_or_create(user=instance, filial=filial)
+
+    elif action == 'post_remove':
+        UserFilial.objects.filter(user=instance, filial_id__in=pk_set).update(is_archived=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_filials_on_create(sender, instance: CustomUser, created, **kwargs):
+    if created:
+        for filial in instance.filial.all():
+            UserFilial.objects.get_or_create(user=instance, filial=filial)
