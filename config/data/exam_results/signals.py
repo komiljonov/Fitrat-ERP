b47623dfdfb81c08b6d9ddfef2d8c20b4e72f6f1
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-15 12:51:09 +0500

    ARCHIVED UPDATE

--- config/data/exam_results/signals.py
+++ config/data/exam_results/signals.py
@@ -1,10 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import MockExamResult, LevelExam, LevelExamResult
-from ..student.groups.models import Group
+from .models import MockExamResult
 from ..student.mastering.models import Mastering
-from ..student.studentgroup.models import StudentGroup
 
 
 @receiver(post_save, sender=MockExamResult)
@@ -18,19 +16,3 @@ def on_create(sender, instance: MockExamResult, created, **kwargs):
             mock=instance.mock,
             test=None,
         )
-
-
-@receiver(post_save, sender=LevelExam)
-def on_create(sender, instance: LevelExam, created, **kwargs):
-    if created:
-        if instance.course and instance.subject and not instance.group:
-            course = instance.course
-
-            group = Group.objects.filter(course__id=course).all()
-
-            students = StudentGroup.objects.filter(group__in=group).all()
-
-            for student in students:
-                res = LevelExamResult.objects.create(
-
-                )
