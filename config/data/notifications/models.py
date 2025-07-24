4c9c4e6a772282ca79baee6f0ac688d20b2d9614
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-24 16:31:42 +0500

    notification firebase update

--- config/data/notifications/models.py
+++ config/data/notifications/models.py
@@ -41,5 +41,3 @@ class Complaint(BaseModel):
 class UserRFToken(BaseModel):
     user : 'CustomUser' = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, null=True,blank=True,related_name='user_rf_token')
     token = models.TextField(null=True,blank=True)
-    def __str__(self):
-        return f"{self.user.full_name}"
