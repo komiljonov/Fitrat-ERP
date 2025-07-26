71e8b2b41010f217208b923b07a7c65399cd42da
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-26 12:32:41 +0500

    notification firebase update

--- config/data/lid/new_lid/views.py
+++ config/data/lid/new_lid/views.py
@@ -442,8 +442,8 @@ class LidStatisticsView(ListAPIView):
 
         first_lesson_all = FirstLLesson.objects.filter(lid__lid_stage_type="ORDERED_LID", **filter,).count()
 
-        new_student = Archived.objects.filter(student__student_stage_type="NEW_STUDENT", **filter).count()
-        active_student = Archived.objects.filter(student__student_stage_type="ACTIVE_STUDENT", **filter).count()
+        new_student = Student.objects.filter(is_archived=True, student__student_stage_type="NEW_STUDENT", **filter).count()
+        active_student = Student.objects.filter(is_archived=True, student__student_stage_type="ACTIVE_STUDENT", **filter).count()
 
         no_debt = Student.objects.filter(is_archived=True, balance__gte=100000, **filter).count()
 
