fc8b56ef325e09b0012aaab30a15980b1ae73327
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-26 10:21:04 +0500

    notification firebase update

--- config/data/dashboard/views.py
+++ config/data/dashboard/views.py
@@ -48,10 +48,13 @@ class DashboardView(APIView):
 
         # Common Filters
         filters = {}
-        if start_date and end_date == "":
+        if start_date:
             filters["created_at__date"] = start_date
+
         if start_date and end_date:
-            filters["created_at__gte", "created_at__lte"] = start_date, end_date
+            filters["created_at__gte"] = start_date
+            filters["created_at__lte"] = end_date
+
         if filial:
             filters["filial"] = filial
 
