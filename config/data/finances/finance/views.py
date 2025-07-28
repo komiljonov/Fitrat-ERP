65291b2ff041714bcda667aa1da6b77a8df8000a
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-28 16:05:03 +0500

    updates

--- config/data/finances/finance/views.py
+++ config/data/finances/finance/views.py
@@ -600,17 +600,16 @@ class FinanceExcel(APIView):
         start_date_str = request.GET.get("start_date")
         end_date_str = request.GET.get("end_date")
 
-
         if start_date_str and end_date_str:
-            start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
-            end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
             finances = finances.filter(
                 created_at__gte=start_date,
-                created_at__lt=end_date + timedelta(days=1) - timedelta(seconds=1)
+                created_at__lt=end_date + timedelta(days=1)
             )
 
-        if start_date_str:
-            start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+        elif start_date_str:
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
             finances = finances.filter(
                 created_at__gte=start_date,
                 created_at__lt=start_date + timedelta(days=1) - timedelta(seconds=1)
