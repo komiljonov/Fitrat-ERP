57da11393b26cffe51ee15a401a0e4dc0a1768f0
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-29 12:50:53 +0500

    updates on dashboard

--- config/data/dashboard/views.py
+++ config/data/dashboard/views.py
@@ -151,7 +151,7 @@ class DashboardView(APIView):
             archived_lid = archived_lid.filter(lids_group__group__course_id=course)
             orders = orders.filter(lids_group__group__course_id=course)
             orders_archived = orders_archived.filter(lids_group__group__course_id=course)
-            first_lesson = first_lesson.filter(students_group__group__course_id=course)
+            first_lesson = first_lesson.filter(group__course__id=course)
             first_lesson_come = first_lesson_come.filter(students_group__group__course_id=course)
             first_lesson_come_archived = first_lesson_come_archived.filter(students_group__group__course_id=course)
 
@@ -323,7 +323,7 @@ class DashboardSecondView(APIView):
             orders = orders.filter(lids_group__group__course_id=course)
             orders_archived = orders_archived.filter(lids_group__group__course_id=course)
             first_lesson = first_lesson.filter(group__course_id=course)
-            first_lesson_come = first_lesson_come.filter(group__course_id=course)
+            first_lesson_come = first_lesson_come.filter(students_group__group__course_id=course)
             first_lesson_come_archived = first_lesson_come_archived.filter(students_group__group__course_id=course)
             first_course_payment = first_course_payment.filter(students_group__group__course_id=course)
             first_course_payment_archived = first_course_payment_archived.filter(
