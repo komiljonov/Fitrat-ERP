e96f26e0232aea8ad552820c433d301d2fe91fc9
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-30 14:22:46 +0500

    updates on dashboard

--- config/data/clickuz/apps.py
+++ config/data/clickuz/apps.py
@@ -1,8 +1,8 @@
-# from django.apps import AppConfig
-#
-#
-# class ClickuzConfig(AppConfig):
-#     name = 'data.clickuz'
-#
-#     def ready(self):
-#         import data.clickuz.signals
+from django.apps import AppConfig
+
+
+class ClickuzConfig(AppConfig):
+    name = 'data.clickuz'
+
+    def ready(self):
+        import data.clickuz.signals
