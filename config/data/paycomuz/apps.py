5cf01e443d3bed156fcbc3c031d2b43d974722f5
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-30 14:22:25 +0500

    updates on dashboard

--- config/data/paycomuz/apps.py
+++ config/data/paycomuz/apps.py
@@ -1,8 +1,8 @@
-# from django.apps import AppConfig
-#
-#
-# class PaycomuzConfig(AppConfig):
-#     name = 'data.paycomuz'
-#
-#     def ready(self):
-#         import data.paycomuz.signals
+from django.apps import AppConfig
+
+
+class PaycomuzConfig(AppConfig):
+    name = 'data.paycomuz'
+
+    def ready(self):
+        import data.paycomuz.signals
