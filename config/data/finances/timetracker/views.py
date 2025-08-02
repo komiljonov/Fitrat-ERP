0b457e121ccc88345b6878df59f6d30ee6a1cd75
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-02 14:58:06 +0500

    updates on dashboard

--- config/data/finances/timetracker/views.py
+++ config/data/finances/timetracker/views.py
@@ -280,7 +280,7 @@ class UserTimeLineBulkUpdateDelete(APIView):
 
 
 class CustomUserPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
 
