281af9e0a071b22d7b682de569bc152e047eefb2
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-02 14:48:42 +0500

    updates on dashboard

--- config/data/finances/timetracker/urls.py
+++ config/data/finances/timetracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import  path
 
 from .models import UserTimeLine
 from .views import AttendanceList, UserTimeLineList, UserTimeLineDetail, TimeTrackerList, \
-    TimeLineBulkCreate, AttendanceDetail, UserTimeLineBulkUpdateDelete
+    TimeLineBulkCreate, AttendanceDetail, UserTimeLineBulkUpdateDelete, EmployeeAttendanceListView
 
 urlpatterns = [
     path("", AttendanceList.as_view()),
@@ -14,4 +14,6 @@ urlpatterns = [
 
     path("timeline/bulk",TimeLineBulkCreate.as_view()),
     path("timeline/bulk/update",UserTimeLineBulkUpdateDelete.as_view()),
+
+    path("all/fixed/",EmployeeAttendanceListView.as_view()),
 ]
