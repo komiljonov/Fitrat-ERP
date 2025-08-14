39dbec9b89368480c0cc8d2499e6f77779ab3e4c
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-14 19:25:25 +0500

    Add filial filters to LidStatisticsView

--- config/data/student/studentgroup/serializers.py
+++ config/data/student/studentgroup/serializers.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.db.models import Count
 from rest_framework import serializers
 
-from config.data.upload.serializers import FileUploadSerializer
+from data.upload.serializers import FileUploadSerializer
 
 from .models import StudentGroup, SecondaryStudentGroup
 from ..attendance.models import Attendance
