1dfd348508948bb01afb525bf71bf81c79a7e1d5
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-19 14:20:30 +0500

    +

--- config/data/student/lesson/views.py
+++ config/data/student/lesson/views.py
@@ -64,6 +64,9 @@ class FistLessonView(ListCreateAPIView):
 
 
     def create(self, request, *args, **kwargs):
+
+        print(request.data)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
