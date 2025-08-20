792099c78e43aeebe25c7cad5d6c86437de1ab8b
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-20 15:56:52 +0500

    timetracker create

--- config/data/student/quiz/views.py
+++ config/data/student/quiz/views.py
@@ -1395,7 +1395,6 @@ class ExamOptionCreate(APIView):
             datas = {
                 "exam_id": exam_id,
                 "student_id": student_id,
-                "option": str(reg.option.id),
                 "status": reg.status,
                 "variation": reg.variation,
                 "is_participating": reg.is_participating,
