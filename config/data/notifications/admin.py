55d16a3b33f33b6f89fd98e1ae1906d4c7c8a723
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-20 17:26:11 +0500

    timetracker create

--- config/data/notifications/admin.py
+++ config/data/notifications/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from data.notifications.models import UserRFToken
+
+
 # Register your models here.
+
+
+@admin.register(UserRFToken)
+class UserRFTokenAdmin(admin.ModelAdmin):
+    list_display = ('user__phone','token')
+    list_filter = ('user__phone','token')
+    search_fields = ('user__phone','token')
