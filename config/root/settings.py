cf8f85200896873ce47e9af2d39d50242758df7b
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-20 17:48:59 +0500

    timetracker create

--- config/root/settings.py
+++ config/root/settings.py
@@ -122,8 +122,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=180),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
