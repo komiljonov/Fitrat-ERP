5d43c93fde55e5c5875eb6c934bfc9bcca730565
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-21 17:27:20 +0500

    timetracker create

--- config/data/parents/apps.py
+++ config/data/parents/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ParentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data.parents'
+
+    def ready(self):
+        import data.parents.signals
