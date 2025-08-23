27e96a7f1dc65cbcbb23428018ca9573b0cd3054
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-23 18:03:15 +0500

    sale student obj has changed

--- config/data/lid/new_lid/tasks.py
+++ config/data/lid/new_lid/tasks.py
@@ -13,29 +13,29 @@ logging.basicConfig(level=logging.INFO)
 
 @shared_task
 def check_daily_leads():
-    overdue_lead = Lid.objects.filter(
-        is_archived=False,
-        updated_at__lte=now() - timedelta(days=3),
-    )
-    for task in overdue_lead:
-        if task.lid_stage_type == "NEW_LID" or task.is_expired:
-            task.is_expired = True
-            task.save()
-            logging.info(f"Lead {task.id} stage updated to 'O'TIB_KETGAN'.")
-
-        if task.call_operator:
-            Notification.objects.create(
-                user=task.call_operator,
-                comment=f"Lead {task.first_name} {task.last_name} - {task.phone_number} bilan aloqa bo'lmaganiga 3 kun bo'ldi!",
-                come_from=task.id,
-            )
-            logging.info(f"Notification created for lead {task.id}.")
-        else:
-            logging.warning(
-                f"No call operator assigned to lead {task.id}. Notification skipped."
-            )
-
-    logging.info("Celery task completed: Checked daily leads.")
+    # overdue_lead = Lid.objects.filter(
+    #     is_archived=False,
+    #     updated_at__lte=now() - timedelta(days=3),
+    # )
+    # for task in overdue_lead:
+    #     if task.lid_stage_type == "NEW_LID" or task.is_expired:
+    #         task.is_expired = True
+    #         task.save()
+    #         logging.info(f"Lead {task.id} stage updated to 'O'TIB_KETGAN'.")
+    #
+    #     if task.call_operator:
+    #         Notification.objects.create(
+    #             user=task.call_operator,
+    #             comment=f"Lead {task.first_name} {task.last_name} - {task.phone_number} bilan aloqa bo'lmaganiga 3 kun bo'ldi!",
+    #             come_from=task.id,
+    #         )
+    #         logging.info(f"Notification created for lead {task.id}.")
+    #     else:
+    #         logging.warning(
+    #             f"No call operator assigned to lead {task.id}. Notification skipped."
+    #         )
+    #
+    # logging.info("Celery task completed: Checked daily leads.")
 
     # === STORY EXPIRATION ===
     overdue_stories = Store.objects.filter(has_expired=False, expired_date__lte=now())
