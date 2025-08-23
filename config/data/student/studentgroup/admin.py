7aac92b3821eeeb29d05e8da8b36cc3cfa4a6f86
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-23 18:44:40 +0500

    sale student obj has changed

--- config/data/student/studentgroup/admin.py
+++ config/data/student/studentgroup/admin.py
@@ -7,7 +7,7 @@ from .models import StudentGroup, SecondaryStudentGroup
 class StudentGroupAdmin(admin.ModelAdmin):
     list_display = ('group','student','lid')
     search_fields = ('group','student','lid')
-    list_filter = ('group',"group__teacher")
+    list_filter = ('group',"group__teacher","is_archived")
 
 
 @admin.register(SecondaryStudentGroup)
