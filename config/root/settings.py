8c254021f0a0cc31cc77efdde53aab75da5765af
shokh_smee <shohjahonobruyev3@gmail.com>
2025-08-25 12:46:43 +0500

    sale student obj has changed

--- config/root/settings.py
+++ config/root/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
