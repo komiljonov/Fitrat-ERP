c6263234ccfe835114cc21cebfa17b4db2548d67
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-28 13:43:22 +0500

    Move send creds with sms to celery

--- config/data/parents/tasks.py
+++ config/data/parents/tasks.py
@@ -1,30 +1,33 @@
 from celery import shared_task
+import requests
 
-from config.data.account.models import CustomUser
+from data.account.models import CustomUser
 from data.student.student.sms import SayqalSms
-from data.parents.models import Relatives
 
 sms = SayqalSms()
 
 
-@shared_task
-def send_creds_to_relatives(relative_id: str, password: str):
+@shared_task(
+    bind=True,
+    autoretry_for=(requests.exceptions.ConnectTimeout,),
+    retry_kwargs={"max_retries": 3, "countdown": 5},
+)
+def send_creds_to_relatives(self, relative_id: str, password: str):
 
     relative = CustomUser.objects.filter(id=relative_id).first()
-
     if relative is None:
         return "Relative not found"
 
     res = sms.send_sms(
         number=relative.phone,
         message=f"""
-                    Fitrat Ota - Onalar uchun ilovasiga muvaffaqiyatli ro‘yxatdan o‘tdingiz!
-    
-                    Login: {relative.phone}
-                    Parol: {password}
-    
-                    Iltimos, ushbu ma’lumotlarni hech kimga bermang. Ilovaga kirib bolangizning natijalarini kuzatishingiz mumkin.
-                    """,
-    )
+            Fitrat Ota - Onalar uchun ilovasiga muvaffaqiyatli ro‘yxatdan o‘tdingiz!
 
-    print(res)
+            Login: {relative.phone}
+            Parol: {password}
+
+            Iltimos, ushbu ma’lumotlarni hech kimga bermang. 
+            Ilovaga kirib bolangizning natijalarini kuzatishingiz mumkin.
+        """,
+    )
+    return res
