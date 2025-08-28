9c93839e903ce740ac74fd54b7b1c18c022ff120
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-28 18:46:40 +0500

    Fix: Make values and names same in FinanceKindTypeChoices

--- config/data/finances/finance/choices.py
+++ config/data/finances/finance/choices.py
@@ -10,12 +10,12 @@ class FinanceKindTypeChoices:
     VOUCHER = "VOUCHER"
 
     CHOICES = [
-        (CASHIER_ACCEPTANCE, "Kassa qabul qilindi"),
-        (CASHIER_HANDOVER, "Kassa topshirildi"),
-        (BONUS, "Bonus"),
-        (SALARY, "Maosh"),
-        (MONEY_BACK, "Money Back"),
-        (LESSON_PAYMENT, "Lesson Payment"),
-        (COURSE_PAYMENT, "Course Payment"),
-        (VOUCHER, "Vaucher"),
+        (CASHIER_ACCEPTANCE, CASHIER_ACCEPTANCE),
+        (CASHIER_HANDOVER, CASHIER_HANDOVER),
+        (BONUS, BONUS),
+        (SALARY, SALARY),
+        (MONEY_BACK, MONEY_BACK),
+        (LESSON_PAYMENT, LESSON_PAYMENT),
+        (COURSE_PAYMENT, COURSE_PAYMENT),
+        (VOUCHER, VOUCHER),
     ]
