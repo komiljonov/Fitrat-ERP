4c6b702f8cf20d6d88abb3ab118a8d987aee0886
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-06 20:37:14 +0500

    Add first lesson to lead

--- config/data/lid/new_lid/urls.py
+++ config/data/lid/new_lid/urls.py
@@ -14,12 +14,12 @@ from .webhook import LidWebhook
 
 urlpatterns = [
     path("", LidListCreateView.as_view(), name="lid_list_create"),
-    path("<uuid:pk>/", LidRetrieveUpdateDestroyView.as_view(), name="lid_retrieve"),
+    path("<uuid:pk>", LidRetrieveUpdateDestroyView.as_view(), name="lid_retrieve"),
     path("no-pg/", LidListNoPG.as_view(), name="lid_list_pg"),
     path("excel/", ExportLidToExcelAPIView.as_view(), name="lid_list_excel"),
-    path("statistic/", LidStatisticsView.as_view(), name="lid_statistic"),
-    path("bulk-update/", BulkUpdate.as_view(), name="bulk_update"),
-    path("document/", ContractGenerateAPIView.as_view(), name="contract_generate"),
-    path("webhook/", LidWebhook.as_view(), name="lid_webhook"),
-    path("stats/", LidStatistics.as_view(), name="lid_statistic"),
+    path("statistic", LidStatisticsView.as_view(), name="lid_statistic"),
+    path("bulk-update", BulkUpdate.as_view(), name="bulk_update"),
+    path("document", ContractGenerateAPIView.as_view(), name="contract_generate"),
+    path("webhook", LidWebhook.as_view(), name="lid_webhook"),
+    path("stats", LidStatistics.as_view(), name="lid_statistic"),
 ]
