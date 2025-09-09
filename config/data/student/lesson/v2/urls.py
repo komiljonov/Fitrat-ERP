4f22e6167095f30a141ff34f4863b99098f0047a
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-09 19:50:24 +0500

    dfg

--- config/data/student/lesson/v2/urls.py
+++ config/data/student/lesson/v2/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
 
+from config.data.student.lesson.v2.views import FirstLessonListCreateAPIView
 
 
-
-
-
-
-
-
-urlpatterns = [
-    
-]
+urlpatterns = [path("first_lessons", FirstLessonListCreateAPIView.as_view())]
--- config/data/student/lesson/v2/views.py
+++ config/data/student/lesson/v2/views.py
@@ -1,12 +1,10 @@
 from rest_framework.generics import ListCreateAPIView
 
-
-
-
-
+from data.student.lesson.models import FirstLLesson
+from data.student.lesson.serializers import FirstLessonSerializer
 
 
 class FirstLessonListCreateAPIView(ListCreateAPIView):
-    
-    
-    
+
+    serializer_class = FirstLessonSerializer
+    queryset = FirstLLesson.objects.all()
