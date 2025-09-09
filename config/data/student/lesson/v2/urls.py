10fba8edd5a7ba4a4a7f3315d100ab3ce077bf92
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-09 19:50:59 +0500

    dfg

--- config/data/student/lesson/v2/urls.py
+++ config/data/student/lesson/v2/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from config.data.student.lesson.v2.views import FirstLessonListCreateAPIView
+from data.student.lesson.v2.views import FirstLessonListCreateAPIView
 
 
 urlpatterns = [path("first_lessons", FirstLessonListCreateAPIView.as_view())]
