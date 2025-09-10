985e8ae0cc4959b98d45494521ef39a772de293f
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-10 19:44:31 +0500

    dfg

--- config/data/employee/apps.py
+++ config/data/employee/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EmployeeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "data.employee"
+
+    def ready(self):
+        import data.employee.signals
