b121a6d8b2e7f68ff28ed52788242c748dacbb75
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-10 18:17:43 +0500

    sdfgsdfg

--- config/data/employee/models.py
+++ config/data/employee/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+from config.data.command.models import BaseModel
 from data.account.models import CustomUser
 from data.employee.manager import EmployeeManager
 
@@ -11,3 +14,31 @@ class Employee(CustomUser):
 
     class Meta:
         proxy = True
+
+
+class EmployeeTransaction(BaseModel):
+
+    employee: "Employee" = models.ForeignKey(
+        "employee.Employee",
+        on_delete=models.CASCADE,
+        related_name="transactions",
+    )
+
+    reason = models.CharField(
+        max_length=255, choices=[("BONUS", "Bonus"), ("FINE", "Fine")]
+    )
+
+    amount = models.DecimalField(decimal_places=2, max_digits=12)
+
+    effective_amount = models.DecimalField(decimal_places=2, max_digits=12)
+
+    def save(self, *args, **kwargs):
+        # Apply business rule before saving
+        if self.reason == "BONUS":
+            self.effective_amount = self.amount
+        elif self.reason == "FINE":
+            self.effective_amount = -self.amount
+        else:
+            self.effective_amount = self.amount  # fallback
+
+        super().save(*args, **kwargs)
--- config/data/employee/views.py
+++ config/data/employee/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import ListCreateAPIView
 
 from data.employee.models import Employee
-from data.account.models import CustomUser
 from data.employee.filters import EmployeesFilter
 from data.employee.serializers import EmployeeSerializer
 
