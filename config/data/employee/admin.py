34923c155599624775af00ad31086165f0cd73a4
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-10 18:22:23 +0500

    dfg

--- config/data/employee/admin.py
+++ config/data/employee/admin.py
@@ -6,9 +6,3 @@ from data.employee.models import Employee
 # Register your models here.
 
 
-@admin.register(Employee)
-class Admin(admin.ModelAdmin):
-
-    list_display = ["id", "full_name", "phone", "role", "balance", "filial"]
-
-    list_filter = ["filial", "role"]
