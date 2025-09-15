df18369757978420e951026fab89ee87e128efeb
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-15 18:57:29 +0500

    dfg

--- config/root/settings.py
+++ config/root/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
