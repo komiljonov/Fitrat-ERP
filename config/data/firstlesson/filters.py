02974801c1fe80964547c4782139c851e55ea1c4
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-16 12:40:48 +0500

    dfg

--- config/data/firstlesson/filters.py
+++ config/data/firstlesson/filters.py
@@ -1,7 +1,11 @@
 import django_filters as filters
 
 from data.department.filial.models import Filial
+from data.department.marketing_channel.models import MarketingChannel
+from data.employee.models import Employee
 from data.firstlesson.models import FirstLesson
+from data.student.course.models import Course
+from data.student.subject.models import Subject
 
 
 class FirstLessonsFilter(filters.FilterSet):
@@ -10,6 +14,36 @@ class FirstLessonsFilter(filters.FilterSet):
 
     status = filters.ChoiceFilter(choices=FirstLesson._meta.get_field("status").choices)
 
+    operator = filters.ModelChoiceFilter(
+        field_name="lead__call_operator",
+        queryset=Employee.objects.filter(role="CALL_OPERATOR"),
+    )
+
+    sales_manager = filters.ModelChoiceFilter(
+        field_name="lead__sales_operator",
+        queryset=Employee.objects.filter(role="ADMINISTRATOR"),
+    )
+
+    subject = filters.ModelChoiceFilter(queryset=Subject.objects.all())
+    course = filters.ModelChoiceFilter(queryset=Course.objects.all())
+
+    marketing_channel = filters.ModelChoiceFilter(
+        queryset=MarketingChannel.objects.all()
+    )
+
+    teacher = filters.ModelChoiceFilter(
+        field_name="group__teacher", queryset=Employee.objects.filter(role="TEACHER")
+    )
+
     class Meta:
         model = FirstLesson
-        fields = ["filial", "status"]
+        fields = [
+            "filial",
+            "status",
+            "operator",
+            "sales_manager",
+            "subject",
+            "course",
+            "marketing_channel",
+            "teacher",
+        ]
