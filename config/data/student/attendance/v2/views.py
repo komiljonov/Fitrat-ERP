259d25be32e83f792ce96ee958c26a86166d58e8
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-17 13:16:37 +0500

    dfg

--- config/data/student/attendance/v2/views.py
+++ config/data/student/attendance/v2/views.py
@@ -117,7 +117,7 @@ class AttendanceGroupStateAPIView(ListAPIView):
         students = group.students.filter(is_archived=False)
 
         return students.select_related("student", "lid").exclude(
-            first_lesson__date__gte=timezone.now().date()
+            first_lesson__date__gt=timezone.now().date()
         )
 
     def get_serializer_context(self):
