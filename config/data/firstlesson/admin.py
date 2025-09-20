7c2ccff3aff5ebccab82e900ed6bee61e75ccc37
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-20 19:04:53 +0500

    dfg

--- config/data/firstlesson/admin.py
+++ config/data/firstlesson/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from data.firstlesson.models import FirstLesson
+
+
 # Register your models here.
+
+
+@admin.register(FirstLesson)
+class Admin(admin.ModelAdmin):
+
+    list_display = [
+        "lead",
+        "group",
+        "date",
+        "status",
+        "lesson_number",
+        "is_archived",
+        "comment",
+        "creator",
+    ]
+
+    search_fields = [
+        "lead__first_name",
+        "lead__last_name",
+        "lead__middle_name",
+        "lead__phone_number",
+    ]
+
+    list_filter = ["status", "group"]
+
+    date_hierarchy = "date"
--- config/data/firstlesson/models.py
+++ config/data/firstlesson/models.py
@@ -116,26 +116,4 @@ class FirstLesson(BaseModel):
             ),
         ]
 
-    class Admin(admin.ModelAdmin):
-
-        list_display = [
-            "lead",
-            "group",
-            "date",
-            "status",
-            "lesson_number",
-            "is_archived",
-            "comment",
-            "creator",
-        ]
-
-        search_fields = [
-            "lead__first_name",
-            "lead__last_name",
-            "lead__middle_name",
-            "lead__phone_number",
-        ]
-
-        list_filter = ["status", "group"]
-
-        date_hierarchy = "date"
+    
