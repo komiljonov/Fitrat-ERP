f8be0f6f5ff84e2f570cc70155d866cbc579dee2
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-20 16:24:34 +0500

    dfg

--- config/data/student/studentgroup/management/commands/test_kick.py
+++ config/data/student/studentgroup/management/commands/test_kick.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from data.student.student.models import Student
 from data.student.studentgroup.tasks import check_for_streak_students
 
 
@@ -9,6 +10,12 @@ class Command(BaseCommand):
 
         print("Running")
 
-        check_for_streak_students()
+        s = Student.objects.filter(id="2097747b-d703-4235-a6d2-896dac2681a9").first()
+
+        sg = s.groups.first()
+
+        print(sg.streak())
+
+        # check_for_streak_students()
 
         print("End")
