05eb7a58f4ebc383549b5204690b5d2761ebbe38
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-22 19:18:25 +0500

    dfg

--- config/data/employee/manager.py
+++ config/data/employee/manager.py
@@ -1,7 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
 
 class EmployeeManager(models.Manager):
 
     def get_queryset(self):
         return super().get_queryset().exclude(role__in=["Student", "Parents"])
+
+    @classmethod
+    def normalize_email(cls, email):
+        """
+        Normalize the email address by lowercasing the domain part of it.
+        """
+        email = email or ""
+        try:
+            email_name, domain_part = email.strip().rsplit("@", 1)
+        except ValueError:
+            pass
+        else:
+            email = email_name + "@" + domain_part.lower()
+        return email
