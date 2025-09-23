597db1e323a75ac6cd7d7a607950658fd21a5650
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-23 18:32:53 +0500

    dfg

--- config/api/urls.py
+++ config/api/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("employees/", include("data.employee.urls")),
     path("first_lessons/", include("data.firstlesson.urls")),
-    path("archives", include("data.archive.urls")),
+    path("archives/", include("data.archive.urls")),
 ]
