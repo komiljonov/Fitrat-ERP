0614611c1a158f47c580425f426807382b1e1125
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-24 17:27:34 +0500

    dfg

--- config/data/employee/methods.py
+++ config/data/employee/methods.py
@@ -12,13 +12,19 @@ class EmployeeMethods:
 
         if self.role == "SERVICE_MANAGER":
 
-            print(f"Service manager: {self.full_name}")
 
             if self.f_svm_bonus_for_each_active_student > 0:
 
-                self.calculate_monthly_salary_for_service_manager()
+                self.calculate_monthly_bonus_for_service_manager()
 
-    def calculate_monthly_salary_for_service_manager(self: "Employee"):
+        if self.role == "FILIAL_Manager":
+
+            print("Filial manager")
+
+            if self.f_managers_bonus_for_each_active_student > 0:
+                self.calculate_monthly_bonus_for_filial_manager()
+
+    def calculate_monthly_bonus_for_service_manager(self: "Employee"):
 
         if self.f_svm_bonus_for_each_active_student == 0:
             return
@@ -27,10 +33,27 @@ class EmployeeMethods:
             is_archived=False, student_stage_type="ACTIVE_STUDENT", service_manager=self
         )
 
-        print(f"Service manager o'quvchilari: {active_students}")
 
         self.transactions.create(
             reason="BONUS_FOR_EACH_ACTIVE_STUDENT",
             amount=self.f_svm_bonus_for_each_active_student * active_students.count(),
-            comment=f"Har bir aktiv o'quvchi uchun bonus. Bonus miqdori: {self.f_svm_bonus_for_each_active_student}, O'quvchilar soni: {active_students.count()}",
+            comment=f"Har bir aktiv o'quvchi uchun bonus. Bonus miqdori: f_managers_bonus_for_each_active_student{self.f_svm_bonus_for_each_active_student}, O'quvchilar soni: {active_students.count()}",
+        )
+
+    def calculate_monthly_bonus_for_filial_manager(self: "Employee"):
+
+        if self.f_managers_bonus_for_each_active_student == 0:
+            return
+
+        active_students = Student.objects.filter(
+            filial__in=self.filial.all(), is_archived=False
+        )
+
+        print(f"Service manager o'quvchilari: {active_students}")
+
+        self.transactions.create(
+            reason="BONUS_FOR_EACH_ACTIVE_STUDENT",
+            amount=self.f_managers_bonus_for_each_active_student
+            * active_students.count(),
+            comment=f"Har bir aktiv o'quvchi uchun bonus. Bonus miqdori: {self.f_managers_bonus_for_each_active_student}, O'quvchilar soni: {active_students.count()}",
         )
