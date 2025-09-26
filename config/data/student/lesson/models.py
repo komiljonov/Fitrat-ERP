0e2f3a147748c32b5d4a1a82d05277fd47bff652
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-26 18:16:22 +0500

    Remove Lesson Object

--- config/data/student/lesson/models.py
+++ config/data/student/lesson/models.py
@@ -16,47 +16,56 @@ from data.student.groups.models import Room
 
 
 # class Lesson(BaseModel):
-class Lesson(BaseModel):
-    name = models.CharField(max_length=100)
-    subject: "Subject" = models.ForeignKey(
-        "subject.Subject", on_delete=models.SET_NULL, null=True, blank=True
-    )
-    group: "Group" = models.ForeignKey(
-        "groups.Group", on_delete=models.CASCADE, related_name="lessons"
-    )
+# class Lesson(BaseModel):
+#     name = models.CharField(max_length=100)
+#     subject: "Subject" = models.ForeignKey(
+#         "subject.Subject",
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         blank=True,
+#     )
+#     group: "Group" = models.ForeignKey(
+#         "groups.Group",
+#         on_delete=models.CASCADE,
+#         related_name="lessons",
+#     )
 
-    theme: "Theme" = models.ForeignKey(
-        "subject.Theme", on_delete=models.SET_NULL, null=True, blank=True
-    )
-    comment = models.TextField(null=True, blank=True)
-    type = models.CharField(
-        choices=[
-            ("Lesson", "Lesson"),
-            ("Repeat", "Repeat"),
-        ],
-        default="Lesson",
-        max_length=100,
-    )
+#     theme: "Theme" = models.ForeignKey(
+#         "subject.Theme",
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         blank=True,
+#     )
 
-    lesson_status = models.CharField(
-        choices=[
-            ("ACTIVE", "Active"),
-            ("FINISHED", "Finished"),
-        ],
-        default="ACTIVE",
-        max_length=20,
-    )
+#     comment = models.TextField(null=True, blank=True)
+#     type = models.CharField(
+#         choices=[
+#             ("Lesson", "Lesson"),
+#             ("Repeat", "Repeat"),
+#         ],
+#         default="Lesson",
+#         max_length=100,
+#     )
 
-    lessons_count = models.IntegerField(default=0)
+#     lesson_status = models.CharField(
+#         choices=[
+#             ("ACTIVE", "Active"),
+#             ("FINISHED", "Finished"),
+#         ],
+#         default="ACTIVE",
+#         max_length=20,
+#     )
 
-    start_time = models.TimeField(null=True, blank=True)  # Start time for the lesson
-    end_time = models.TimeField(null=True, blank=True)  # End time for the lesson
-    day = models.DateField(null=True, blank=True)  # Specific day of the lesson
+#     lessons_count = models.IntegerField(default=0)
 
-    def __str__(self):
-        return (
-            f"Lesson {self.name}      {self.group}      Room {self.group.room_number}"
-        )
+#     start_time = models.TimeField(null=True, blank=True)  # Start time for the lesson
+#     end_time = models.TimeField(null=True, blank=True)  # End time for the lesson
+#     day = models.DateField(null=True, blank=True)  # Specific day of the lesson
+
+#     def __str__(self):
+#         return (
+#             f"Lesson {self.name}      {self.group}      Room {self.group.room_number}"
+#         )
 
 
 class FirstLLesson(BaseModel):
