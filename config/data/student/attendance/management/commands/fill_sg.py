5927ef9cb8b8400602f49a2938e1db98dab4bca2
shukurulloxkomiljonov <me@komiljonov.uz>
2025-09-29 16:15:45 +0500

    dfrg

---
+++ config/data/student/attendance/management/commands/fill_sg.py
@@ -0,0 +1,24 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+
+from data.student.attendance.models import Attendance
+from data.student.studentgroup.models import StudentGroup
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any) -> str | None:
+
+        attendances = Attendance.objects.filter(student_group=None)
+
+        for attendance in tqdm(attendances):
+
+            sg = StudentGroup.objects.filter(
+                student=attendance.student,
+                lid=attendance.lead,
+                group=attendance.group,
+                is_archived=False,
+            )
+
+            attendance.student_group = sg
+            attendance.save()
