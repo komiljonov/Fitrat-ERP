fca4f04cfbae426f266b6c0d922f8e526c83d1e0
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-03 19:19:49 +0500

    dfg

--- config/data/student/student/v2/filters.py
+++ config/data/student/student/v2/filters.py
@@ -1,8 +1,9 @@
-from logging import Filter
 import django_filters as filters
 from django.db.models import Q
+
+from data.department.filial.models import Filial
 
 
 class StudentFilter(filters.FilterSet):
 
-    filial = filters.ModelChoiceFilter(queryset=Filter.objects.all())
+    filial = filters.ModelChoiceFilter(queryset=Filial.objects.all())
