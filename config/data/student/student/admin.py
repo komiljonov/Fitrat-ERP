c482518625a465d03c8b81ec5e02a652a7723412
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-03 19:10:25 +0500

    dfg

--- config/data/student/student/admin.py
+++ config/data/student/student/admin.py
@@ -6,13 +6,18 @@ from .models import Student, FistLesson_data
 # Register your models here.
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ( 'first_name', 'last_name','phone',"student_stage_type")
-    search_fields = ('first_name', 'last_name','phone')
-    list_filter = ('first_name', 'last_name','phone',"student_stage_type")
+    list_display = ("first_name", "last_name", "phone", "student_stage_type")
+    search_fields = ("first_name", "last_name", "phone")
+    list_filter = ["student_stage_type", "is_archived"]
 
 
 @admin.register(FistLesson_data)
 class FistLessonDataAdmin(admin.ModelAdmin):
-    list_display = ( 'teacher__full_name', 'group__name','lesson_date',"lid__first_name")
-    list_filter = ('teacher__full_name', 'group__name','lesson_date')
-    search_fields = ('teacher__full_name', 'group__name','lesson_date')
+    list_display = (
+        "teacher__full_name",
+        "group__name",
+        "lesson_date",
+        "lid__first_name",
+    )
+    list_filter = ("teacher__full_name", "group__name", "lesson_date")
+    search_fields = ("teacher__full_name", "group__name", "lesson_date")
