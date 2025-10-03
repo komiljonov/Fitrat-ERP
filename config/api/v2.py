16854c87888e0c8dc34917ac05cf0e590e21d5eb
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-03 19:07:50 +0500

    dfg

--- config/api/v2.py
+++ config/api/v2.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("attendances/", include("data.student.attendance.v2.urls")),
     path("studentgroups/", include("data.student.studentgroup.v2.urls")),
     path("lessons/", include("data.student.lesson.v2.urls")),
-    path("students/", include("data.student.students.v2.urls")),
+    path("students/", include("data.student.student.v2.urls")),
 ]
