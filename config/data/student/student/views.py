0eef5e4271b5f455e8fae80b46bf3539c4390315
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-07 13:21:19 +0500

    dfgdfgdfg

--- config/data/student/student/views.py
+++ config/data/student/student/views.py
@@ -176,7 +176,7 @@ class StudentListView(FilialRestrictedQuerySetMixin, ListCreateAPIView):
             except ValueError:
                 pass  # Handle invalid date format, if necessary
 
-        return queryset.order_by("-created_at").select_related(
+        return queryset.order_by("is_frozen", "-created_at").select_related(
             "photo", "service_manager", "sales_manager"
         )
 
