a83f050f7036e29ae2d35cc02d2b27123540b8f0
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-07 12:40:33 +0500

    dfg

--- config/root/urls.py
+++ config/root/urls.py
@@ -35,6 +35,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("api/v2/", include("api.v2")),
     path("api/", include("api.urls")),
@@ -81,6 +86,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("sentry-debug/", trigger_error),
 ]
 
 urlpatterns += [
