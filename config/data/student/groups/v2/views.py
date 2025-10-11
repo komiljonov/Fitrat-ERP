8bdb4ed4d247ca0e9f367d09355c12e5367c67f8
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-11 17:24:05 +0500

    dfg

--- config/data/student/groups/v2/views.py
+++ config/data/student/groups/v2/views.py
@@ -1,11 +1,11 @@
 from rest_framework.generics import ListAPIView
 
 from data.student.groups.models import Group
+from data.student.groups.v2.serializers import GroupSerializer
 
 
 class GroupListAPIView(ListAPIView):
 
     queryset = Group.objects.all()
-    
-    
+
     serializer_class = GroupSerializer
