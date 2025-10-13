b2f212678e9d4081c6db66a59a1348eef8a62005
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-13 17:36:49 +0500

    dfg

--- config/data/student/groups/admin.py
+++ config/data/student/groups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from data.student.groups.models import Day, Group, Room, SecondaryGroup
+from data.student.subject.models import Theme
 
 
 # Register your models here.
@@ -17,6 +18,22 @@ class RoomAdmin(admin.ModelAdmin):
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("name", "teacher", "status", "room_number", "price")
     search_fields = ("name", "teacher", "status", "room_number", "price")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "start_theme":
+            obj = getattr(request, "_obj_", None)
+            qs = Theme.objects.all()
+            if obj and obj.level_id:
+                qs = qs.filter(level_id=obj.level_id)
+            if obj and obj.course_id:
+                qs = qs.filter(course_id=obj.course_id)
+            kwargs["queryset"] = qs
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_object(self, request, object_id, from_field=None):
+        obj = super().get_object(request, object_id, from_field)
+        request._obj_ = obj
+        return obj
 
 
 @admin.register(SecondaryGroup)
