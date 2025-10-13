4224d927c69fa889353e47e8a64cde5b8fdd52f9
shukurulloxkomiljonov <me@komiljonov.uz>
2025-10-13 14:42:32 +0500

    Themes/no-pg remove pagination

--- config/data/student/subject/v2/views.py
+++ config/data/student/subject/v2/views.py
@@ -8,6 +8,7 @@ from data.student.subject.v2.filters import ThemeFilter
 class ThemeNoPgListAPIView(ListAPIView):
 
     serializer_class = ThemeSerializer.only("id", "title", "description")
+    pagination_class = None
 
     queryset = Theme.objects.filter(is_archived=False)
 
